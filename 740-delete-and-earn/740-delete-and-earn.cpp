f6076d9baeee9cb03d2e380921d5dfd2d43f35ef
Bharat2911 <96980832+Bharat2911@users.noreply.github.com>
2022-04-02 01:27:31 +0530

    Time: 12 ms (54.39%), Space: 16.3 MB (11.57%) - LeetHub

---
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int deleteAndEarn(vector<int>& nums) {
+        
+        vector<int>dp(10001,0);
+        vector<int>sum(10001,0);
+        
+        for(auto i:nums)
+        {
+            sum[i]+=i;
+        }
+        dp[0]=0;
+        dp[1]=sum[1];
+        
+        //similar to house robber 
+        
+        for(int i=2;i<10001;i++)
+        {
+            //if take the current elemnt cant take the adjacent elemnt
+            //else simply take the adjacent and max of both will be our aswer
+            dp[i]=max(sum[i]+dp[i-2],dp[i-1]);
+        }
+        return dp[10001-1];
+    }
+};
