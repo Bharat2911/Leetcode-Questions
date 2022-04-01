c0161ab7515b5046cfbe1e6382db26b5c93af3e5
Bharat2911 <96980832+Bharat2911@users.noreply.github.com>
2022-04-02 01:10:50 +0530

    Time: 15 ms (20.22%), Space: 12.1 MB (13.29%) - LeetHub

---
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -0,0 +1,39 @@
+class Solution
+{
+    public:
+        int f(int idx, bool isbuy, vector<int> &prices, int n, vector<vector< int>> &dp)
+        {
+           	//base case
+
+            if (idx >= n)
+            {
+                return 0;
+            }
+
+            int profit = 0;
+
+            if (dp[idx][isbuy] != -1) return dp[idx][isbuy];
+
+           	//choice diagram
+            if (isbuy)
+            {
+               	//take or not take two choices here
+                profit = max(-prices[idx] + f(idx + 1, false, prices, n, dp), 0 + f(idx + 1, true, prices, n, dp));
+            }
+            else
+            {
+               	//whter to sell or not sell again two choices here
+                profit = max(prices[idx] + f(idx + 2, true, prices, n, dp), 0 + f(idx + 1, false, prices, n, dp));
+            }
+
+            return dp[idx][isbuy] = profit;
+        }
+    int maxProfit(vector<int> &prices)
+    {
+        int n = prices.size();
+
+        vector<vector < int>> dp(n, vector<int> (2, -1));
+
+        return f(0, true, prices, prices.size(), dp);
+    }
+};
