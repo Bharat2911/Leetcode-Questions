d6ad5a911fdc710ec9d66d6f1ec96e203f7d49bd
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-04-03 20:57:29 +0530

    Time: 18 ms (37.08%), Space: 8.1 MB (71.34%) - LeetHub

--- 224-basic-calculator/224-basic-calculator.cpp
+++ 224-basic-calculator/224-basic-calculator.cpp
@@ -1,62 +1,63 @@
-class Solution {
-public:
-    int calculate(string s) {
-         int n=s.length();
-        
-        stack<int>st;
-        
-        int sign=+1;
-        int curr_num=0;
-        int ans=0;
-        
-        for(int i=0;i<n;i++)
+class Solution
+{
+    public:
+        int calculate(string s)
         {
-            if(isdigit(s[i]))//means if it is a integer
-                
+            int n = s.length();
+
+            stack<int> st;
+
+            int sign = +1;
+            int curr_num = 0;
+            int ans = 0;
+
+            for (int i = 0; i < n; i++)
             {
-                 curr_num=s[i]-'0';
-                
-                while(i+1<n and isdigit(s[i+1]) )//jab tak integr mile usko number banate jao
+                if (s[i] >= '0'
+                    and s[i] <= '9')	//means if it is a integer
+
                 {
-                    curr_num=curr_num*10+(s[i+1]-'0');
-                    i++;
+                    curr_num = s[i] - '0';
+
+                    while (i + 1 < n and(s[i + 1] >= '0'
+                            and s[i + 1] <= '9'))	//jab tak integr mile usko number banate jao
+                    {
+                        curr_num = curr_num *10 + (s[i + 1] - '0');
+                        i++;
+                    }
+                   	//couple of basic steps
+                    curr_num = curr_num * sign;
+                    ans += curr_num;
+                    curr_num = 0;
+                    sign = +1;
                 }
-                //couple of basic steps
-                curr_num=curr_num*sign;
-                ans+=curr_num;
-                curr_num=0;
-                sign=+1;
+                else if (s[i] == '+')
+                {
+                    sign = +1;
+                }
+                else if (s[i] == '-')
+                {
+                    sign = -1;
+                }
+                else if (s[i] == '(')	//this is the core step
+                {
+                    st.push(ans);
+                    st.push(sign);
+                    ans = 0;
+                    sign = +1;
+                }
+                else if (s[i] == ')')	//pop out steps
+                {
+                    int prev_sign = st.top();
+                    st.pop();
+
+                    int prev_ans = st.top();
+                    st.pop();
+
+                    ans = ans * prev_sign;
+                    ans = ans + prev_ans;
+                }
             }
-            else if(s[i]=='+')
-            {
-                sign=+1;
-            }
-            else if(s[i]=='-')
-            {
-                sign=-1;
-                
-            }
-            else if(s[i]=='(')//this is the core step
-            {
-                st.push(ans);
-                st.push(sign);
-                ans=0;
-                sign=+1;
-            }
-            else if(s[i]==')')//pop out steps
-            {
-                int prev_sign=st.top();
-                st.pop();
-                
-                int prev_ans=st.top();
-                st.pop();
-                
-                ans=ans*prev_sign;
-                ans=ans+prev_ans;
-                
-            }
+            return ans;
         }
-        return ans;
-    
-    }
 };
