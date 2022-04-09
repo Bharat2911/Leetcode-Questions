fd9bdc8f21c572b956819ebd3b180dab18d23804
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-04-10 01:01:14 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (78.62%) - LeetHub

--- 394-decode-string/394-decode-string.cpp
+++ 394-decode-string/394-decode-string.cpp
@@ -1,43 +1,43 @@
 class Solution {
 public:
-    string solve(string&s,int &i)
+    //makign the recursive function
+    string solve(string &s,int &i)
     {
+        int n=s.length();
         string ans;
         
-         while(i<s.length() and s[i]!=']')
-         {
-             if(s[i]>='0' and s[i]<='9')//means there is a digit
-             {
-                 int k=0;
-                 while(i<s.length() and isdigit(s[i]))
-                     k=k*10+s[i++]-'0';
-                 i++;
-                 
-                 ///recursive call 
-                 string temp=solve(s,i);
-                 
-                 //apppend k times that we get from the recursive function
-                 while(k-- > 0)
-                     ans+=temp;
-                 i++;
-               
-                 
-                 
-             }
-             else
-             {
-                 ans+=s[i];
-                 i++;
-                 
-             }
-         }
+        while(i<n and s[i]!=']')
+        {
+            if(isdigit(s[i]))
+            {    
+                int k=0;
+                while(i<n and isdigit(s[i]))
+                    k=k*10+(s[i++]-'0');
+                i++;
+                
+                //recursive call bracket ke andar se answer nikal ke dega
+                string temp=solve(s,i);
+                
+                
+                
+                while(k-- > 0)
+                    ans+=temp;//append k times
+                i++;
+                
+                    
+            }
+            else 
+            {
+                ans+=s[i];
+                i++;
+            }
+        }
         return ans;
     }
     string decodeString(string s) {
         
-        int n=s.length();
+        //usign the recursion here
         int i=0;
-        
         return solve(s,i);
     }
 };
