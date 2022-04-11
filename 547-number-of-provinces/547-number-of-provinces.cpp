f9296e6f5ae7a2b7b3838baac6aaf6af197dd00a
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-04-11 23:31:39 +0530

    Time: 28 ms (67.72%), Space: 14.9 MB (11.95%) - LeetHub

---
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    void dfs(int start,vector<vector<int>>& grid,vector<int>&vis)
+    {
+        vis[start]=1;
+          int n=grid.size();
+        
+        vector<int>adj;
+        
+        
+            for(int j=0;j<n;j++)
+            {
+                int x=grid[start][j];
+                
+                if(x==1)
+                {
+                    adj.push_back(j);
+                }
+            }
+    
+        for(auto itr:adj)
+        {
+            if(!vis[itr])
+            {
+                vis[itr]=1;
+                dfs(itr,grid,vis);
+            }
+        }
+    }
+    int findCircleNum(vector<vector<int>>& grid) {
+        
+        //->make the adj list
+        // /->count the components of the grapg
+        
+        int n=grid.size();
+        
+//         vector<int>adj;
+        
+//         for(int i=0;i<n;i++)
+//         {
+//             for(int j=0;j<n;j++)
+//             {
+//                 int x=grid[i][j];
+                
+//                 if(x==1)
+//                 {
+//                     adj.push_back(i);
+//                 }
+//             }
+//         }
+        //by dfs we will count the component
+        vector<int>vis(n,0);
+        
+        int count=0;
+        
+        for(int i=0;i<n;i++)
+        {
+            if(!vis[i])
+            {
+                count++;
+                dfs(i,grid,vis);
+            }
+        }
+        return count;
+    }
+};
