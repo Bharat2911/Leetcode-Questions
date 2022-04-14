ba82603dde6eea7e570e09c511b1a011254f0168
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-04-14 16:05:30 +0530

    Time: 14 ms (75.37%), Space: 12.1 MB (7.42%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -2,20 +2,44 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         
+        // ****this is the kattar brute force approach****
+//         int n=nums.size();
+//         vector<int>ans;
         
-        int n=nums.size();
+//         for(int i=0;i<n;i++)
+//         {
+//             for(int j=i+1;j<n;j++)
+//             {
+//                 if(nums[i]+nums[j]==target)
+//                 {
+//                     ans.push_back(i);
+//                     ans.push_back(j);
+//                     break;
+//                 }
+//             }
+//         }
+//         return ans;
+        
+        //optimising the above approach using the map
+        unordered_map<int,int>map;
         vector<int>ans;
-        
-        for(int i=0;i<n;i++)
+        for(int i=0;i<nums.size();i++)
         {
-            for(int j=i+1;j<n;j++)
+            map[nums[i]]=i;
+        }
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            int count=target-nums[i];
+            if(map.find(count)!=map.end())
             {
-                if(nums[i]+nums[j]==target)
+                if(i!=map[count])
                 {
-                    ans.push_back(i);
-                    ans.push_back(j);
-                    break;
+                      ans.push_back(i);
+                ans.push_back(map[count]);
+                break;
                 }
+               
             }
         }
         return ans;
