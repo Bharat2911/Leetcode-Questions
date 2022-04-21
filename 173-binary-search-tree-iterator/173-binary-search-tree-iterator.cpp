6a6ca9dae954100988093dc42b09ca900e478993
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-04-21 12:07:07 +0530

    Time: 43 ms (38.17%), Space: 24.1 MB (87.51%) - LeetHub

--- 173-binary-search-tree-iterator/173-binary-search-tree-iterator.cpp
+++ 173-binary-search-tree-iterator/173-binary-search-tree-iterator.cpp
@@ -1,65 +1,57 @@
 /**
- *Definition for a binary tree node.
- *struct TreeNode {
- *   int val;
- *   TreeNode * left;
- *   TreeNode * right;
- *   TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- *};
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
  */
-class BSTIterator
-{
-    public:
-
-       	//using queue we will try to do this
+class BSTIterator {
+public:
+    //this is an optimised approach 
+    //t.c=O(1);
+    //s.c=O(h);
     
-    //time complexity of this is o(n);->using queue
+    stack<TreeNode*>st;
     
-    //time complexity O(h),h->height of a tree->using stack;
-    
-    
-    
-        queue<int> q;
-
-    void inorder(TreeNode *root)
+    BSTIterator(TreeNode* root) {
+        
+        push_left(root);
+        
+    }
+    void push_left(TreeNode*root)
     {
-        if (root == NULL)
+        //pushes left element into the stack
+        while(root)
         {
-            return;
+            st.push(root);
+            root=root->left;
         }
-        inorder(root->left);
-        q.push(root->val);
-
-        inorder(root->right);
+        
     }
-    BSTIterator(TreeNode *root)
-    {
-
-        inorder(root);
+    int next() {
+        
+        TreeNode*top=st.top();
+        st.pop();
+        
+        push_left(top->right);
+        
+        return top->val;
     }
-
-    int next()
-    {
-
-        int top = q.front();
-        q.pop();
-
-        return top;
-    }
-
-    bool hasNext()
-    {
-
-        if (q.empty()) return false;
-        return true;
+    
+    bool hasNext() {
+        
+        if(!st.empty())return true;
+        return false;
     }
 };
 
 /**
- *Your BSTIterator object will be instantiated and called as such:
- *BSTIterator* obj = new BSTIterator(root);
- *int param_1 = obj->next();
- *bool param_2 = obj->hasNext();
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator* obj = new BSTIterator(root);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
  */
