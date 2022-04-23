da699494d75d382496ed647514f0262651a5141e
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-04-23 13:25:08 +0530

    Time: 3 ms (50.14%), Space: 7.8 MB (25.58%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,32 +1,24 @@
 class Solution
 {
     public:
-        int rob(vector<int> &nums)
+        int solve(int idx, vector<int> &nums,vector<int>&dp)
         {
+           	//base case
+            if (idx < 0) return 0;
+            if(dp[idx]!=-1)return dp[idx];
+            
+           	//choice diagram 
 
-            int n = nums.size();
-            if (n == 0)
-            {
-                return 0;
-            }
-            if (n == 1)
-            {
-                return nums[0];
-            }
-            if (n == 2)
-            {
-                return max(nums[0], nums[1]);
-            }
-            int dp[n];
+            int pick = nums[idx] + solve(idx - 2, nums,dp);	//agar le rha hu to adjacent elel nahi lung
+            int not_pick = solve(idx - 1, nums,dp);
 
-            dp[0] = nums[0];
-            dp[1] = max(nums[0], nums[1]);
-
-            for (int i = 2; i < n; i++)
-            {
-                dp[i] = max(nums[i] + dp[i - 2], dp[i - 1]);
-            }
-
-            return dp[n - 1];
+            return dp[idx]=max(pick, not_pick);
         }
+    int rob(vector<int> &nums)
+    {
+        int n = nums.size();
+        vector<int>dp(n+1,-1);
+        
+        return solve(n - 1, nums,dp);
+    }
 };
