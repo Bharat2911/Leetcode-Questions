ef4de69d77c308089c26e5fe43e5a493860d6fcd
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-04-23 14:33:04 +0530

    Time: 226 ms (90.74%), Space: 24.5 MB (49.88%) - LeetHub

--- 454-4sum-ii/454-4sum-ii.cpp
+++ 454-4sum-ii/454-4sum-ii.cpp
@@ -3,28 +3,28 @@ public:
     int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
         
         unordered_map<int,int>map;
-        int count=0;
         
         for(auto itr1:nums1)
         {
-            for(auto itr2:nums2)
-            {
-                int sum_ab=itr1+itr2;
-                
-                map[sum_ab]++;//increase frequency of sum_ab
-            }
+             for(auto itr2:nums2)
+             {
+                int sum1=itr1+itr2;
+                 map[sum1]++;
+             }
         }
+           int count=0;
         
         for(auto itr3:nums3)
         {
             for(auto itr4:nums4)
             {
-               int sum_cd=itr3+itr4;
-                int sum_ab=-sum_cd;
+               int  sum2=itr3+itr4;
+                int sum1=-sum2;
                 
-               count+=map[sum_ab];
+                count+=map[sum1];
             }
         }
         return count;
+                
     }
 };
