9f8112e34cb9897e502d5aa1e99db783a10746ba
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-05-03 12:17:09 +0530

    Time: 84 ms (12.19%), Space: 18.3 MB (12.84%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,49 +1,53 @@
 class Solution {
 public:
-    void dfs(int i,int j,int row,int col,vector<vector<char>>& grid,vector<vector<int>>&vis)
+    void bfs(int i,int j,int row,int col,vector<vector<char>>& grid,vector<vector<bool>>& vis)
     {
-        //base case
-        //out of bound conditiojn
-        if(i<0 || i>=row || j<0 || j>=col)
+        queue<pair<int,int>>q;
+        
+        q.push({i,j});
+        vis[i][j]=true;
+        
+        int dx[]={0,1,0,-1};
+        int dy[]={1,0,-1,0};
+        
+        while(!q.empty())
         {
-            return;
-        }
-        else if(grid[i][j]=='0')
-        {
-            return;
+            int x=q.front().first;
+            int y=q.front().second;
+            q.pop();
+            
+            for(int idx=0;idx<4;idx++)
+            {
+                int newi=x+dx[idx];
+                int newj=y+dy[idx];
+                
+                
+                if(newi>=0 and newi<row and newj>=0 and newj<col and !vis[newi][newj] and grid[newi][newj]=='1')
+                {
+                    vis[newi][newj]=true;
+                    q.push({newi,newj});
+                }
+            }
         }
         
-        if(!vis[i][j])
-        {
-            vis[i][j]=1;
-            
-            //send in all the dour direction so that  we get the one in that direction it automativcally marked visited
-            dfs(i+1,j,row,col,grid,vis);
-            dfs(i-1,j,row,col,grid,vis);
-            dfs(i,j+1,row,col,grid,vis);
-            dfs(i,j-1,row,col,grid,vis);
-            
-        }
     }
     int numIslands(vector<vector<char>>& grid) {
         
         int row=grid.size();
         int col=grid[0].size();
         
-        int count=0;
+        vector<vector<bool>>vis(row,vector<bool>(col,false));
         
-        vector<vector<int>>vis(row,vector<int>(col,0));
+        int count=0;
         
         for(int i=0;i<row;i++)
         {
             for(int j=0;j<col;j++)
             {
-                if(!vis[i][j] and grid[i][j]=='1')
+                if(!vis[i][j] and grid[i][j]=='1' )
                 {
                     count++;
-                    
-                    dfs(i,j,row,col,grid,vis);
-                    
+                    bfs(i,j,row,col,grid,vis);
                 }
             }
         }
