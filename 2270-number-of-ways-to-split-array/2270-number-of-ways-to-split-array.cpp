1a9c97de6c39c684ce1837c0516170ed828f752c
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-05-15 09:33:28 +0530

    Time: 207 ms (66.67%), Space: 99.7 MB (33.33%) - LeetHub

--- 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.cpp
+++ 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.cpp
@@ -3,23 +3,34 @@ public:
     int waysToSplitArray(vector<int>& nums) {
         
      int n=nums.size();
-        vector< long long int> pre(n,0),suf(n,0);
+        vector<long long int>pre(n,0);
+        vector<long long int>suff(n,0);
+        
         pre[0]=nums[0];
-        suf[n-1]=nums[n-1];
-        for(long long int i=1;i<n;i++){
+        suff[n-1]=nums[n-1];
+        
+        for(long long int i=1;i<n;i++)
+        {
             pre[i]=pre[i-1]+nums[i];
+          
         }
-        for(long long int i=n-2;i>=0;i--){
-            suf[i]=suf[i+1]+nums[i];
+        
+        for(long long int i=n-2;i>=0;i--)
+        {
+            suff[i]=suff[i+1]+nums[i];
+            
         }
-       int ans=0;
-        for(long long int i=0;i<n-1;i++){
-          long long int  left=pre[i];
-            long long int  right=suf[i+1];
+        
+        int count=0;
+        
+        for(long long int i=0;i<n-1;i++)
+        {
+            long long int  left=pre[i];
+            long long int  right=suff[i+1];
             if(left>=right){
-                ans++;
+                count++;
             }
         }
-        return ans;
+        return count;
     }
 };
