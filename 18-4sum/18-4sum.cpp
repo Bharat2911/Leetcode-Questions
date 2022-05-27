c0d2a36bb008b7839dca4df11796cf71cb3d07d5
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-05-28 01:59:29 +0530

    Time: 929 ms (6.67%), Space: 106.9 MB (5.02%) - LeetHub

--- 18-4sum/18-4sum.cpp
+++ 18-4sum/18-4sum.cpp
@@ -2,42 +2,54 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         
+        //now optimised it 
+        //t.c->O(n^3);
         int n=nums.size();
+        
+        vector<vector<int>>ans;
         sort(nums.begin(),nums.end());
+        //couple of base case
         
-        if(nums.size()<4)return{};
+        if(n<4)return ans;
         
-        vector<vector<int>>res;
+       
+        
         for(int i=0;i<n-3;i++)
         {
             for(int j=i+1;j<n-2;j++)
             {
                 int k=j+1;
-                int z=n-1;
+                int x=n-1;
                 
-                while(k<z)
+                while(k<x)
                 {
-                    int sum=(long long)nums[i]+nums[j]+nums[k]+nums[z];
+                    int sum=(long long )nums[i]+nums[j]+nums[k]+nums[x];
                     
-                    if(sum<target)
+                    
+                    if(sum==target)
                     {
+                        ans.push_back({nums[i],nums[j],nums[k],nums[x]});
+                        
                         k++;
+                        x--;
                     }
                     else if(sum>target)
                     {
-                        z--;
+                        x--;
                     }
                     else
                     {
-                        res.push_back({nums[i],nums[j],nums[k],nums[z]});
                         k++;
-                        z--;
                     }
+                    
+                    
+                    
                 }
             }
         }
-        sort(res.begin(),res.end());
-        res.erase(unique(res.begin(), res.end()), res.end());
-        return res;
+        sort(ans.begin(),ans.end());
+          ans.erase(unique(ans.begin(), ans.end()), ans.end());
+        return ans;
+        
     }
 };
