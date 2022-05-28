6a45d43dc71463988623d5c5cb899eb6f5f68b08
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-05-28 16:49:42 +0530

    Time: 99 ms (24.98%), Space: 42.2 MB (96.97%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
-    int maxScore(vector<int>& A, int B) {
+    int maxScore(vector<int>& nums, int k) {
         
-        //store the sum of first k elemnts 
-        //then delet element from last from sum and elemnt from last
-       int n=A.size();
-    int sum=0;
-    int res=0;
-
-    for(int i=0;i<B;i++ )
-    {
-        res+=A[i];
-    }
-    sum=res;
-
-
-    for(int i=0;i<B;i++)
-    {
-       sum-=A[B-1-i];
-       sum+=A[n-1-i];
-
-       res=max(res,sum);
-    }
-    return res;
+        //t.c ->O(n);
+        //s.cO(1);
+        
+          int n=nums.size();
+        int sum=0;
+        int ans=0;
+        
+        for(int i=0;i<k;i++)
+        {
+            sum+=nums[i];
+        }
+        ans=sum;
+        
+        for(int i=0;i<k;i++)
+        {
+           sum-=nums[k-1-i];
+            sum+=nums[n-i-1];
+            
+            
+            ans=max(ans,sum);
+        }
+        return ans;
     }
 };
