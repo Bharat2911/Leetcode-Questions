54db14f5dd19b3a134eb51a3f2c12e54edef158a
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-06-06 14:32:04 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (5.87%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.cpp
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -1,39 +1,65 @@
-class Solution {
-public:
-    string solve(string&s)
-    {
-        stack<char>st;
-        string ans;
-        
-        for(int i=0;i<s.length();i++)
+class Solution
+{
+    public:
+        bool backspaceCompare(string s, string t)
         {
-            if(s[i]!='#')
+
+            int n1 = s.length();
+            int n2 = t.length();
+
+            stack<char> st1;
+            stack<char> st2;
+
+            for (int i = 0; i < n1; i++)
             {
-                st.push(s[i]);
-            }
-            else
-            {
-                if(st.size()>0)
+                if (s[i] != '#')
                 {
-                    st.pop();
+                    st1.push(s[i]);
+                }
+                else
+                {
+                    if(st1.size()>0)
+                    {
+                        st1.pop();
+                    }
+                    
                 }
             }
+            for (int i = 0; i < n2; i++)
+            {
+                if (t[i] != '#')
+                {
+                    st2.push(t[i]);
+                }
+                else
+                {
+                    if(st2.size()>0)
+                    {
+                        st2.pop();
+                    }
+                    
+                }
+            }
+            string ans1 = "";
+            string ans2 = "";
+            while (!st1.empty())
+            {
+                char ch = st1.top();
+                st1.pop();
+
+                ans1.push_back(ch);
+            }
+            while (!st2.empty())
+            {
+                char ch = st2.top();
+                st2.pop();
+
+                ans2.push_back(ch);
+            }
+            if (ans1 == ans2)
+            {
+                return true;
+            }
+            return false;
         }
-        while(!st.empty())
-        {
-            ans+=st.top();
-            st.pop();
-        }
-        return ans;
-    }
-    bool backspaceCompare(string s, string t) {
-        
-        //stack implimentation
-        string s1=solve(s);
-        string s2=solve(t);
-        
-        if(s1==s2)return true;
-        return false;
-        
-    }
 };
