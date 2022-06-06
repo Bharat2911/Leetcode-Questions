a4e4d5650c97ee2017f10a6de2b62551a9b95576
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-06-06 19:53:46 +0530

    Time: 324 ms (11.99%), Space: 85.1 MB (71.09%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.cpp
+++ 739-daily-temperatures/739-daily-temperatures.cpp
@@ -2,24 +2,27 @@ class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temp) {
         
-        //similar to next greater element but int this stack is storing the index
         int n=temp.size();
-        stack<int>st;//stack is storing the index
         vector<int>ans(n);
+        
+        stack<int>st;
+        
         ans[n-1]=0;
         st.push(n-1);
         
         for(int i=n-2;i>=0;i--)
         {
-            while(!st.empty() and temp[st.top()]<=temp[i])
+            while(!st.empty() and temp[i]>=temp[st.top()])
             {
                 st.pop();
             }
+            
             int x=st.empty()?0:(st.top()-i);
             ans[i]=x;
-            st.push(i);
             
+            st.push(i);
         }
+        
         return ans;
     }
 };
