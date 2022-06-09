601c975b42fa3a2cb19f35a91500c1934cf9a474
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-06-09 13:21:53 +0530

    Time: 8 ms (96.97%), Space: 15.5 MB (93.11%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -4,30 +4,63 @@ public:
         
         // o(n),O(n); ->map approach hai
         
-        unordered_map<int,int>map;
-        int n=arr.size();
+//         unordered_map<int,int>map;
+//         int n=arr.size();
         
-        for(int i=0;i<n;i++)
-        {
-            map[arr[i]]=i+1;
-        }
+//         for(int i=0;i<n;i++)
+//         {
+//             map[arr[i]]=i+1;
+//         }
+//         vector<int>ans;
+        
+//         for(int i=0;i<n;i++)
+//         {
+//                int x=tar-arr[i];
+            
+//             if(map.find(x)!=map.end())
+//             {
+//                 ans.push_back(i+1);
+//                 ans.push_back(map[x]);
+//                 break;
+                
+//             }
+            
+//         }
+//         return ans;
+        
+        
+        //since araay is sorted so use that concpet here 
+        //means two pointer approach 
+        //T.C O(N)
+        //S.C O(1) //constant space lega two pointer approach
+        
         vector<int>ans;
         
-        for(int i=0;i<n;i++)
+        int n=arr.size();
+        
+        int i=0;
+        int j=n-1;
+        
+        while(i<j and j>=0)
         {
-               int x=tar-arr[i];
-            
-            if(map.find(x)!=map.end())
+            if(arr[i]+arr[j]==tar)
             {
                 ans.push_back(i+1);
-                ans.push_back(map[x]);
+                ans.push_back(j+1);
+                i++;
+                j--;
                 break;
-                
             }
-            
+            else if(arr[i]+arr[j]>tar)
+            {
+                j--;
+            }
+            else
+            {
+                i++;
+            }
         }
+        
         return ans;
-        
-        
     }
 };
