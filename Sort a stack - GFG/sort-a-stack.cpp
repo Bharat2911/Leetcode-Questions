1f06e5c30a0e35d767d0cccd6099b80b06ff7c2a
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-06-09 19:25:26 +0530

    Added solution - LeetHub

---
+++ Sort a stack - GFG/sort-a-stack.cpp
@@ -0,0 +1,84 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+class SortedStack{
+public:
+	stack<int> s;
+	void sort();
+};
+
+void printStack(stack<int> s)
+{
+    while (!s.empty())
+    {
+        printf("%d ", s.top());
+       	s.pop();
+    }
+    printf("\n");
+}
+
+int main()
+{
+int t;
+cin>>t;
+while(t--)
+{
+	SortedStack *ss = new SortedStack();
+	int n;
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+	int k;
+	cin>>k;
+	ss->s.push(k);
+	}
+	ss->sort();
+	printStack(ss->s);
+}
+}// } Driver Code Ends
+
+
+/*The structure of the class is
+class SortedStack{
+public:
+	stack<int> s;
+	void sort();
+};
+*/
+
+/* The below method sorts the stack s 
+you are required to complete the below method */
+void insert(stack<int>&s,int x)
+{
+    if(s.size()==0 || s.top()<x)
+    {
+        s.push(x);
+    }
+    else
+    {
+        if(s.size()>0)
+        {
+            int a=s.top();
+            s.pop();
+            insert(s,x);
+            s.push(a);
+        }
+    }
+    
+}
+void reverse(stack<int>&s)
+{
+    if(s.size()>0)
+    {
+        int x=s.top();
+        s.pop();
+        reverse(s);
+        insert(s,x);
+    }
+}
+void SortedStack :: sort()
+{
+   //Your code here
+   reverse(s);
+}
