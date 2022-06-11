31c2fe5eed7d19f0679212f9d3939ea9a9c45e7e
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-06-11 17:16:03 +0530

    Time: 53 ms (44.10%), Space: 71.5 MB (11.86%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -12,17 +12,15 @@ class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         
-        //since already storedin reverse order so dont need to reverse it
-        
+        ListNode*dummy_node=new ListNode(-1);
+        ListNode*ptr=dummy_node;
         
         int carry=0;
         
-        ListNode*dummy=new ListNode(0);
-        ListNode*temp=dummy;
-        
-        while(l1!=NULL || l2!=NULL ||carry)
+        while(l1 || l2 || carry)
         {
             int sum=0;
+            
             if(l1!=NULL)
             {
                 sum+=l1->val;
@@ -37,10 +35,9 @@ public:
             sum+=carry;
             carry=sum/10;
             
-            temp->next=new ListNode(sum%10);
-            temp=temp->next;
+            ptr->next= new ListNode(sum%10);
+            ptr=ptr->next;
         }
-        return dummy->next;
-        
+        return dummy_node->next;
     }
 };
