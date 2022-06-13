d20d41105e53d3b9dd7f374de8aac86f3254315a
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-06-13 13:59:46 +0530

    Time: 34 ms (90.29%), Space: 71.4 MB (47.81%) - LeetHub

--- 445-add-two-numbers-ii/445-add-two-numbers-ii.cpp
+++ 445-add-two-numbers-ii/445-add-two-numbers-ii.cpp
@@ -1,42 +1,42 @@
 /**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
+ *Definition for singly-linked list.
+ *struct ListNode {
+ *    int val;
+ *    ListNode * next;
+ *    ListNode() : val(0), next(nullptr) {}
+ *    ListNode(int x) : val(x), next(nullptr) {}
+ *    ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *};
  */
-class Solution {
-public:
-    ListNode*reverse(ListNode*head)
-    {
-        ListNode*curr=head;
-        ListNode*prev=NULL;
-        ListNode*next;
-        
-        while(curr!=NULL)
+class Solution
+{
+    public:
+        ListNode* reverse(ListNode *head)
         {
-            next=curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=next;
+            ListNode *curr = head;
+            ListNode *prev = NULL;
+            ListNode * next;
+
+            while (curr != NULL)
+            {
+                next = curr->next;
+                curr->next = prev;
+                prev = curr;
+                curr = next;
+            }
+           	return prev;
         }
-        return prev;
-    }
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        
-        l1=reverse(l1);
+    ListNode* addTwoNumbers(ListNode *l1, ListNode *l2)
+    {
+
+       l1= reverse(l1);
         l2=reverse(l2);
         
-         
+        ListNode*dummyNode=new ListNode(-1);
+        ListNode*ptr=dummyNode;
         int carry=0;
         
-        ListNode*dummy=new ListNode(0);
-        ListNode*temp=dummy;
-        
-        while(l1!=NULL || l2!=NULL ||carry)
+        while(l1 || l2 || carry)
         {
             int sum=0;
             if(l1!=NULL)
@@ -49,15 +49,13 @@ public:
                 sum+=l2->val;
                 l2=l2->next;
             }
-            
             sum+=carry;
             carry=sum/10;
             
-            temp->next=new ListNode(sum%10);
-            temp=temp->next;
+            ptr->next=new ListNode(sum%10);
+            ptr=ptr->next;
         }
-        dummy->next=reverse(dummy->next);
-        
-        return dummy->next;
+        dummyNode->next=reverse(dummyNode->next);
+        return dummyNode->next;
     }
 };
