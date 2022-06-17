0d2212f171b349deb9a4c80cce2c2ecfbf752362
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-06-17 18:09:39 +0530

    Time: 20 ms (38.19%), Space: 21.3 MB (55.06%) - LeetHub

---
+++ 968-binary-tree-cameras/968-binary-tree-cameras.cpp
@@ -0,0 +1,72 @@
+/**
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
+ */
+class Solution
+{
+    public:
+       	//we will be going in bottom,ups manner
+       	//means we will be doing the post order traversal
+
+       	//for each node we will be checking the four cases
+       	//node has_camera
+       	//node need_camers
+       	//node not_needed_camera
+       	//node covered
+
+        int camera_count = 0;
+
+    int has_camera = 0;
+    int need_camera = 1;
+    int not_needed_camera = 2;
+    int covered = 3;
+
+    int minCameraCover(TreeNode *root)
+    {
+
+       camera_count= solve(root)==need_camera?camera_count+1:camera_count;
+
+        return camera_count;
+    }
+    int solve(TreeNode *root)
+    {
+        if (root == NULL)
+        {
+           	//no camera is needed
+            return not_needed_camera;
+        }
+       	//leaf
+        if (root->left == NULL and root->right == NULL)
+        {
+           	//since camera will be needed in this case
+            return need_camera;
+        }
+
+        int left = solve(root->left);
+        int right = solve(root->right);
+
+        if (left == need_camera || right == need_camera)
+        {
+            camera_count++;
+           	//since now the particular node will be having the camera
+            return has_camera;
+        }
+        if (left == has_camera || right == has_camera)
+        {
+           	//every node is covered now 
+            return covered;
+        }
+        return need_camera;
+        
+        //Time_complexity =O(n)//since we aretraversing the nodes onlyonce
+        //space_complexity==log(n);
+        
+    }
+};
