7bada26057b18535877b6b948fbff6e788d25dce
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-06-21 20:07:22 +0530

    Time: 343 ms (75.43%), Space: 8.1 MB (36.11%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -1,54 +1,56 @@
-class Solution
-{
-    public:
-        bool solve(int i, int j, int idx, vector<vector < char>> &board, string &word,vector<vector<bool>>&vis)
+class Solution {
+public:
+    bool solve(int index,int i,int j,vector<vector<char>>&board,string&word,vector<vector<bool>>&vis)
+    {
+        //base case
+        if(index==word.length())
         {
-           	//base cases
-            int row=board.size();
-            int col=board[0].size();
+            return true;
+        }
+        int row=board.size();
+        int col=board[0].size();
+        
+        //itrate in the four direction and check 
+        int dx[4]={0,1,0,-1};
+        
+        int dy[4]={1,0,-1,0};
+        
+        vis[i][j]=true;
+        
+        
+        for(int idx=0;idx<4;idx++)
+        {
+            int newi=i+dx[idx];
+            int newj=j+dy[idx];
             
-            if (idx == word.size() ) return true;
-            
-            int dx[]={0,1,0,-1};
-            int dy[]={1,0,-1,0};
-            
-            vis[i][j]=true;
-            
-            for(int x=0;x<4;x++)
+            if(newi>=0 and newi<row and newj>=0 and newj<col and !vis[newi][newj]  and board[newi][newj]==word[index])
             {
-                int newi=i+dx[x];
-                int newj=j+dy[x];
-                
-                if(newi>=0 and newi<row and newj>=0 and newj<col and !vis[newi][newj] and board[newi][newj]==word[idx])
+                if(solve(index+1,newi,newj,board,word,vis))
                 {
-                    if(solve(newi, newj,idx+1,board,word,vis))
-                    {
-                        return true;
-                    }
+                    return true;
                 }
             }
-            vis[i][j]=false;//this is backtraking step
-            
-            return false;
-            
-            
-           
         }
-    bool exist(vector<vector < char>> &board, string word)
-    {
-
-       	//trying the bfs algo on this 
-        int row = board.size();
-        int col = board[0].size();
+        
+        vis[i][j]=false;//back traking stepto try all [possble way]
+        
+        
+        return false;
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        
+        int row=board.size();
+        int col=board[0].size();
+        
         vector<vector<bool>>vis(row,vector<bool>(col,false));
         
-        for (int i = 0; i < row; i++)
+        for(int i=0;i<row;i++)
         {
-            for (int j = 0; j < col; j++)
+            for(int j=0;j<col;j++)
             {
-                if (board[i][j] == word[0])
+                if(board[i][j]==word[0])
                 {
-                    if (solve(i, j, 1, board, word,vis))
+                    if (solve(1,i,j,board,word,vis))
                     {
                         return true;
                     }
