a8d80c1ee2ae391b6cefb5733767403494d2faba
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-06-22 16:06:51 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (63.14%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,39 +1,42 @@
 class Solution {
 public:
-    int solve(int i,int j,vector<vector<int>>&grid,vector<vector<int>>&dp)
+    int solve(int i,int j,int row,int col,vector<vector<int>>&grid,vector<vector<int>>&dp)
     {
-        int row=grid.size();
-        int col=grid[0].size();
+        //base case
+        if(i<0 || j<0 || i>=row || j>=col)
+        {
+            return 0;
+        }
+        if(grid[i][j]==1)
+        {
+            return 0;
+        }
         
-        //basec conditions
-         if(  i>=row ||   j>=col)return 0;
+        if(i==row-1 and j==col-1)
+        {
+            return 1;
+            
+        }
         
-        if(grid[i][j]==1)return 0;
         if(dp[i][j]!=-1)return dp[i][j];
         
+        int a=solve(i+1,j,row,col,grid,dp);
         
-        if(i==row-1 and j==col-1)return 1;
-        
-        int a=solve(i+1,j,grid,dp);
-        int b=solve(i,j+1,grid,dp);
+        int b=solve(i,j+1,row,col,grid,dp);
         
         return dp[i][j]=a+b;
-        
     }
     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
-        
-        //use the recursion simple
-        //try all possible path
-        
         int row=grid.size();
         int col=grid[0].size();
         
         vector<vector<int>>dp(row+1,vector<int>(col+1,-1));
         
-        int starti=0;
-        int startj=0;
+        int i=0;
+        int j=0;
         
-        return solve(starti,startj,grid,dp);
+        return solve(i,j,row,col,grid,dp);
+        
         
     }
 };
