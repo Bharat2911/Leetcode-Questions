53b596296bea79c6779e177fa42bd43130c8a14a
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-06-23 16:33:36 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (43.59%) - LeetHub

--- 299-bulls-and-cows/299-bulls-and-cows.cpp
+++ 299-bulls-and-cows/299-bulls-and-cows.cpp
@@ -1,36 +1,38 @@
-class Solution
-{
-    public:
-        string getHint(string secret, string guess)
+class Solution {
+public:
+    string getHint(string s, string g) {
+        
+        //x A y B
+        //x->bulls
+        //y->coes
+        
+        vector<int>arr1(26,0);
+        vector<int>arr2(26,0);
+        
+        int bulls=0;
+        int cows=0;
+        
+        for(int i=0;i<s.length();i++)
         {
-
-           
-            vector<int> arr1(11, 0);
-            vector<int> arr2(11, 0);
-
-            int bulls = 0;
-            int cows = 0;
-
-            for (int i = 0; i < secret.size(); i++) 
+            if(s[i]==g[i])
             {
-                if(secret[i]==guess[i])
-                {
-                    bulls++;
-                }
-                else
-                {
-                   //heare is the problem
-                    arr1[secret[i]-'0']++;
-                    arr2[guess[i]-'0']++;
-                   
-                }
+                bulls++;
+                
             }
-            for(int i=0;i<10;i++)
+            else
             {
-                cows+=min(arr1[i],arr2[i]);
+                arr1[s[i]-'0']++;
+                arr2[g[i]-'0']++;
+                
             }
-            string ans=to_string(bulls)+'A'+to_string(cows)+'B';
-            
-            return ans;
         }
+        for(int i=0;i<26;i++)
+        {
+            cows+=min(arr1[i],arr2[i]);
+        }
+        
+        string ans=to_string(bulls)+'A'+to_string(cows)+'B';
+        
+        return ans;
+    }
 };
