e8d91e9f7963fb170c949b1de4ef5f69a4421aea
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-06-29 20:02:57 +0530

    Time: 158 ms (63.45%), Space: 53.8 MB (53.83%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
@@ -1,40 +1,34 @@
-class Solution
-{
-    public:
-        int furthestBuilding(vector<int> &arr, int b, int l)
+class Solution {
+public:
+    int furthestBuilding(vector<int>& arr, int b, int l) {
+        
+        //if think greedly to bade distance ke liye will use ladder and for small distance will use bricks
+         priority_queue <int, vector<int>, greater<int> > pq;
+         int bsum=0;
+        
+        
+        for(int i=0;i<arr.size()-1;i++)
         {
-
-            int n = arr.size();
-
-           	//min heap
-            int briksum=0;
+            int diff=arr[i+1]-arr[i];
             
-            priority_queue<int, vector < int>, greater<int>> pq;
-           
-            
-
-            for (int i = 0; i < n - 1; i++)
+            if(diff>0)
             {
-                int diff = arr[i + 1] - arr[i];
-
-                if (diff > 0)
-                {
-                    pq.push(diff);
-                }
-
-                if (pq.size() > l)
-                {
-                    briksum+=pq.top();
-                    pq.pop();
-                    
-                }
-
-                if (b < briksum)
-                {
-                    return i;
-                }
+                pq.push(diff);
             }
-           	//if we reach the end then 
-            return n - 1;
+            if(pq.size()>l)
+            {
+                b-=pq.top();
+                pq.pop();
+                
+            }
+            if(b<0)
+            {
+                return i;
+            }
         }
+        //otherwise i will reach the end so return n-1;
+        
+        return arr.size()-1;
+        
+    }
 };
