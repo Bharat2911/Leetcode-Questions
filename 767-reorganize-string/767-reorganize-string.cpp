5e6603a08613c47292e992c71a27ebdbb868ace9
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-06-30 14:16:30 +0530

    Time: 10 ms (6.76%), Space: 6.3 MB (33.85%) - LeetHub

--- 767-reorganize-string/767-reorganize-string.cpp
+++ 767-reorganize-string/767-reorganize-string.cpp
@@ -1,62 +1,65 @@
-class Solution
-{
-    public:
-        string reorganizeString(string s)
+class Solution {
+public:
+    string reorganizeString(string s) {
+        
+        //greedy thinking hain
+        //arrange the max frequency char alternate follwed by second max frequency then third
+        //max frequency 
+        int n=s.length();
+        
+        //max heap
+        priority_queue<pair<int,char>>pq;
+        
+        //map to store then freq of each char 
+        unordered_map<char,int>map;
+        
+        for(auto itr:s)
         {
-
-            int n = s.length();
-            priority_queue<pair<int, char>> pq;
-
-            unordered_map<char, int> map;
-            for (auto itr: s)
-            {
-                map[itr]++;
-            }
-
-            for (auto itr: map)
-            {
-                pq.push({ itr.second,
-                    itr.first });
-            }
-            string ans = "";
-            if (pq.size() == 0) return ans;
-
-            while (pq.size() > 1)
-            {
-                int f1 = pq.top().first;
-                char ele1 = pq.top().second;
-
-              
-
-                ans += ele1;
-                
-                pq.pop();
-                int f2 = pq.top().first;
-                char ele2 = pq.top().second;
-
-                ans += ele2;
-                pq.pop();
-                
-
-                if (f1>1)
-                {
-                    pq.push({f1-1,ele1});
-                }
-                if(f2>1)
-                {
-                    pq.push({f2-1,ele2});
-                }
-
-            }
-            if(pq.size()==1 and pq.top().first==1)
-            {
-                ans+=pq.top().second;
-            }
-            if(pq.size()==1 and pq.top().first>1)
-            {
-                return "";
-                
-            }
-            return ans;
+            map[itr]++;
         }
+        
+        //push in the pq
+        
+        for(auto itr:map)
+        {
+            pq.push({itr.second,itr.first});
+        }
+         string ans="";
+        
+        if(pq.size()==0)return ans;
+        
+        while(pq.size()>1)
+        {
+            int f1=pq.top().first;
+            char char1=pq.top().second;
+            pq.pop();
+            
+            ans+=char1;
+            
+            int f2=pq.top().first;
+            char char2=pq.top().second;
+            pq.pop();
+            
+            
+            ans+=char2;
+            
+            if(f1-1>0)
+            {
+                pq.push({f1-1,char1});
+            }
+            if(f2-1>0)
+            {
+                pq.push({f2-1,char2});
+            }
+        }
+        if(pq.size()==1 and pq.top().first==1)
+        {
+            ans+=pq.top().second;
+        }
+        if(pq.size()==1 and pq.top().first>1)
+        {
+            return "";
+        }
+        return ans;
+    }
 };
