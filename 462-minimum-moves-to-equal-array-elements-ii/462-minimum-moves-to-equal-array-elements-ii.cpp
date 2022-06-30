01797d0ffe0605e2f4a3c2bcd916fd921f26007b
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-06-30 11:12:25 +0530

    Time: 25 ms (14.20%), Space: 10.9 MB (15.75%) - LeetHub

---
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int minMoves2(vector<int>& nums) {
+        
+       /*
+         Approach 
+         
+         i will pick  a element from array itself ,try to make all the other elements of araay
+         equal to that element
+         
+         i will pick a element in such a way that we get the minimum moves
+         
+         so will sort the array in increasing order and pick the middel of the array and try to 
+         make all the other elements of the array equal to mid element
+         
+         WHY MID ELEMENT ??
+         beacuse in the sorted array the mid element will have the least distance from all the
+         other elements.
+         
+         
+         
+         
+       */
+        //sort 
+        sort(nums.begin(),nums.end());
+        
+        int ans=0;
+        
+        int n=nums.size();
+        
+        int mid=nums[n/2];
+        
+        for(int i=0;i<n;i++)
+        {
+            ans+=abs(nums[i]-mid);
+        }
+        
+        return ans;
+    }
+};
