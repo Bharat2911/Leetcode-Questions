6c2808ba94e412f7a413e59d484344ae55caf627
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-07-03 17:10:53 +0530

    Time: 81 ms (38.00%), Space: 16 MB (50.90%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -1,53 +1,54 @@
-class Solution {
-public:
-    
-    
-    int solve(int i,int j,vector<vector<int>>& matrix,vector<vector<int>>& vis)
+class Solution
+{
+    public:
+        int solve(int i, int j, vector<vector < int>> &matrix, vector< vector< int>> &dp)
+        {
+           	//in four direcxtions 
+            int row = matrix.size();
+            int col = matrix[0].size();
+            int dx[4] = { 0,
+                1,
+                0,
+                -1
+            };
+            int dy[4] = { 1,
+                0,
+                -1,
+                0
+            };
+            int ans = 1;
+            
+            if (dp[i][j] != -1) return dp[i][j];
+
+            for (int idx = 0; idx < 4; idx++)
+            {
+                int newi = i + dx[idx];
+                int newj = j + dy[idx];
+
+                if (newi >= 0 and newi < row and newj >= 0 and newj < col and matrix[newi][newj] > matrix[i][j])
+                {
+                    int count = 1 + solve(newi, newj, matrix,dp);
+
+                    ans = max(ans, count);
+                }
+            }
+            return dp[i][j]=ans;
+        }
+    int longestIncreasingPath(vector<vector < int>> &matrix)
     {
-         int row=matrix.size();
-        int col=matrix[0].size();
-        
-        if(vis[i][j]!=-1)return vis[i][j];
-        
-        int dx[]={0,1,0,-1};
-        int dy[]={1,0,-1,0};
-        
-        int count=1;
-        
-        for(int idx=0;idx<4;idx++)
+
+        int row = matrix.size();
+        int col = matrix[0].size();
+        int ans = 0;
+        vector<vector < int>> dp(row, vector<int> (col, -1));
+
+        for (int i = 0; i < row; i++)
         {
-            int newi=i+dx[idx];
-            int newj=j+dy[idx];
-            
-            
-            if(newi>=0 and newi<row and newj>=0 and newj<col  and matrix[newi][newj]>matrix[i][j])
+            for (int j = 0; j < col; j++)
             {
-                
-                
-                count=max(count,1+solve(newi,newj,matrix,vis));
+                ans = max(ans, solve(i, j, matrix, dp));
             }
         }
-        return  vis[i][j]=count;
-    }
-    int longestIncreasingPath(vector<vector<int>>& matrix) {
-        
-        //simple dfs and dp
-        int row=matrix.size();
-        int col=matrix[0].size();
-        
-        vector<vector<int>>vis(row,vector<int>(col,-1));
-        int ans=0;
-        
-        for(int i=0;i<row;i++)
-        {
-            for(int j=0;j<col;j++)
-            {
-               
-                   ans=max(ans,solve(i,j,matrix,vis));
-                
-            }
-        }
-        
         return ans;
     }
 };
