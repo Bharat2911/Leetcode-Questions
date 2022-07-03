a80b7dd9ff3cf43f067fc563b8e070419ec6b15c
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-07-03 15:01:48 +0530

    Time: 12 ms (18.28%), Space: 6.6 MB (43.05%) - LeetHub

--- 299-bulls-and-cows/299-bulls-and-cows.cpp
+++ 299-bulls-and-cows/299-bulls-and-cows.cpp
@@ -2,33 +2,30 @@ class Solution {
 public:
     string getHint(string s, string g) {
         
-        //x A y B
-        //x->bulls
-        //y->coes
+        vector<int>for_s(10,0);
+        vector<int>for_g(10,0);
         
-        vector<int>arr1(26,0);
-        vector<int>arr2(26,0);
+        int n=s.length();
         
         int bulls=0;
         int cows=0;
         
-        for(int i=0;i<s.length();i++)
+        for(int i=0;i<n;i++)
         {
             if(s[i]==g[i])
             {
                 bulls++;
-                
             }
             else
             {
-                arr1[s[i]-'0']++;
-                arr2[g[i]-'0']++;
-                
+                //update in vector
+                for_s[s[i]-'0']++;
+                for_g[g[i]-'0']++;
             }
         }
-        for(int i=0;i<26;i++)
+        for(int i=0;i<10;i++)
         {
-            cows+=min(arr1[i],arr2[i]);
+            cows+=min(for_s[i],for_g[i]);
         }
         
         string ans=to_string(bulls)+'A'+to_string(cows)+'B';
