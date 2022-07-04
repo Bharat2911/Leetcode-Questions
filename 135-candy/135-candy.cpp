f5c3a948121e317d6565aee2b579277d041bb6d2
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-07-04 12:43:31 +0530

    Time: 39 ms (36.17%), Space: 18.3 MB (23.59%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -4,28 +4,29 @@ public:
         
         int n=nums.size();
         
-        vector<int>ans(n,1);
+        vector<int>ans_l(n,1);
+        vector<int>ans_r(n,1);
         
         for(int i=1;i<n;i++)
         {
             if(nums[i]>nums[i-1] )
             {
-                ans[i]=ans[i-1]+1;
+                ans_l[i]=ans_l[i-1]+1;
                 
             }
         }
         for(int i=n-2;i>=0;i--)
         {
-            if(nums[i]>nums[i+1] and ans[i]<=ans[i+1])
+            if(nums[i]>nums[i+1] )
             {
-                ans[i]=ans[i+1]+1;
+                ans_r[i]=ans_r[i+1]+1;
             }
         }
         int sum=0;
-        for(auto itr:ans)
-        {
-            sum+=itr;
-        }
+       for(int i=0;i<n;i++)
+       {
+           sum+=max(ans_l[i],ans_r[i]);
+       }
         
         return sum;
     }
