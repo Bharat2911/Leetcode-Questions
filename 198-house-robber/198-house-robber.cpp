85d09a7db7815f7863d1319ec35c48ed6a1f31b5
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-07-07 12:58:40 +0530

    Time: 4 ms (34.93%), Space: 7.7 MB (75.64%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,25 +1,40 @@
-class Solution
-{
-    public:
-        int solve(int idx, vector<int> &nums,vector<int>&dp)
-        {
-           	//base case
-            if(idx==0)return nums[idx];
-            if (idx < 0) return 0;
-            if(dp[idx]!=-1)return dp[idx];
-            
-           	//choice diagram 
-
-            int pick = nums[idx] + solve(idx - 2, nums,dp);	//agar le rha hu to adjacent elel nahi lung
-            int not_pick = solve(idx - 1, nums,dp);
-
-            return dp[idx]=max(pick, not_pick);
-        }
-    int rob(vector<int> &nums)
+class Solution {
+public:
+    
+    int solve(int index,vector<int>&nums,int n,vector<int>&dp)
     {
-        int n = nums.size();
-        vector<int>dp(n+1,-1);
+        //bae case
+        if(index>n-1)
+        {
+            return 0;
+        }
         
-        return solve(n - 1, nums,dp);
+        if(dp[index]!=-1)return dp[index];
+        
+        //two options 
+        
+        int pick = nums[index]+solve(index+2,nums,n,dp);//i cant choose the ele next to it
+        
+        int not_pick=solve(index+1,nums,n,dp);
+        
+        
+        return dp[index]=max(pick,not_pick);
+        
+    }
+    int rob(vector<int>& nums) {
+        
+        //we will be writing a recursion function
+        
+        //concept -. if we choose the curr ele then we cant take the ele just next to i t
+        
+        int index=0;
+        
+        int n=nums.size();
+        
+        vector<int>dp(n,-1);
+        
+        
+        return solve(index,nums,n,dp);
+        
     }
 };
