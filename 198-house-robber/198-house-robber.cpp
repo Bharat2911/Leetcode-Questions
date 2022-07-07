c25d0ffe84248c6236b54b9c159b16f4a558dbef
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-07-07 13:17:42 +0530

    Time: 5 ms (22.66%), Space: 7.6 MB (75.64%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -20,16 +20,16 @@ public:
             
         }
         
-        int dp[n];
+        int dp[2];
         
         dp[0]=nums[0];
         dp[1]=max(nums[0],nums[1]);
         
         for(int i=2;i<n;i++)
         {
-            dp[i]=max(nums[i]+dp[i-2],dp[i-1]);
+            dp[i%2]=max(nums[i]+dp[(i-2)%2],dp[(i-1)%2]);
         }
         
-        return dp[n-1];
+        return dp[(n-1)%2];
     }
 };
