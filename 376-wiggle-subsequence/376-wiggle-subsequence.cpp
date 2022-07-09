5a3d4ff7ad1c1ff162571732ead4d45da37006a0
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-07-09 13:21:42 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (69.39%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -2,31 +2,27 @@ class Solution {
 public:
     int wiggleMaxLength(vector<int>& nums) {
         
+        //up= down+1
+        //down =up+1;
+        
         int n=nums.size();
         
-        //usign the concept up=down+1
-        //ans down=up+1;
+        int up=1;
+        int down=1;
         
-        int up=0;
-        int down=0;
-        int ans=0;
-        
-        //in the end we will add one to our final answer becuse in each case we will include the first char
         for(int i=1;i<n;i++)
         {
-            if(nums[i]>nums[i-1])//up_hill case
+            if(nums[i]>nums[i-1])//up hill case it is
             {
                 up=down+1;
             }
-            else if(nums[i]<nums[i-1])//down_hill case
+            else if(nums[i]<nums[i-1])//down hill case it is
             {
                 down=up+1;
+                
             }
         }
         
-        ans=max(down,up);
-        
-        return ans+1;
-        
+        return max(up,down);
     }
 };
