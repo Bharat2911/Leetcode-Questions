cebb4543cc839b627b9e34d1128a12152585f0b9
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-07-12 17:48:51 +0530

    Time: 38 ms (49.51%), Space: 16.1 MB (62.75%) - LeetHub

--- 437-path-sum-iii/437-path-sum-iii.cpp
+++ 437-path-sum-iii/437-path-sum-iii.cpp
@@ -1,54 +1,48 @@
 /**
- *Definition for a binary tree node.
- *struct TreeNode {
- *   int val;
- *   TreeNode * left;
- *   TreeNode * right;
- *   TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- *};
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
  */
-class Solution
-{
-    public:
-        int count = 0;
-    void solve(TreeNode *root, int tsum, long long sum)
+class Solution {
+public:
+    int count=0;
+    void solve(TreeNode*root,int ts,long long sum)
     {
-        if (root == NULL)
+        if(root==NULL)
         {
             return;
         }
-
-        sum += root->val;
-
-        if (sum == tsum)
-        {
-            count++;
-        }
-
-        solve(root->left, tsum, sum);
-        solve(root->right, tsum, sum);
+        
+        
+        
+        sum+=root->val;
+        
+        if(sum==ts)count++;
+        
+        solve(root->left,ts,sum);
+        solve(root->right,ts,sum);
+        
     }
-    int pathSum(TreeNode *root, int targetSum)
-    {
-
-       	//in this we have to do a preorder traversal 
-       	//so after traversing each node i will be checking the sum
-        if (root == NULL)
+    int pathSum(TreeNode* root, int targetSum) {
+        
+        if(root==NULL)
         {
             return 0;
+            
         }
-        long long sum = 0;
-
-        solve(root, targetSum, sum);
-
-       	//send the same function to left and right so each and every node will be checked
-
-        pathSum(root->left, targetSum);
-
-        pathSum(root->right, targetSum);
-
+        long long sum=0;
+        
+        solve(root,targetSum,sum);
+        
+        pathSum(root->left,targetSum);
+        pathSum(root->right,targetSum);
+        
         return count;
     }
 };
