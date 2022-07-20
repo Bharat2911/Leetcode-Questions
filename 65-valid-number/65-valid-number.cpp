15b09c064af335fe115df8b5daa64a1147056124
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-07-20 21:08:51 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (99.79%) - LeetHub

---
+++ 65-valid-number/65-valid-number.cpp
@@ -0,0 +1,52 @@
+class Solution
+{
+    public:
+        bool isNumber(string s)
+        {
+
+            int n = s.length();
+            bool digitseen = false;
+            bool dotseen = false;
+            bool eseen = false;
+            int plusminus=0;
+            
+            for(int i=0;i<n;i++)
+            {
+                if(s[i]>='0' and s[i]<='9')
+                {
+                    digitseen=true;
+                }
+                else if(s[i]=='+' || s[i]=='-')
+                {
+                    if(plusminus==2  || i==n-1 ||(i>0 and  s[i-1]!='e' and s[i+1]!='E') )
+                    {
+                        return false;
+                        
+                    }
+                    
+                    plusminus++;
+                }
+                else if(s[i]=='.')
+                {
+                    if(eseen || (i==n-1 and !digitseen) || dotseen)//since we cant have more than two dots
+                    {
+                        return false;
+                    }
+                    dotseen=true;
+                }
+                else if(s[i]=='e'  || s[i]=='E')
+                {
+                    if(eseen || i==n-1 || !digitseen)//we cant have two e and without digit we cant have e 
+                    {
+                        return false;
+                    }
+                    eseen=true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+};
