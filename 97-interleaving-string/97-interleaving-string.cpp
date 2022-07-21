7ea7b54bc068ee7ed2192f085a49a6726d9b678f
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-07-21 16:10:37 +0530

    Time: 3 ms (82.59%), Space: 6.3 MB (86.77%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,38 +1,53 @@
-class Solution
-{
-    public:
-        int dp[101][101];
-    bool solve(string &s1, string &s2, string &s3, int i, int j, int k)
+class Solution {
+public:
+    int dp[101][101];
+    bool solve(string&s1,string&s2,string&s3,int i,int j,int k)
     {
-
-        if (i + j != k) return false;
-
-        if (k == 0)
+        //base case
+        if(i==s1.length() and j==s2.length() and k==s3.length())
         {
             return true;
         }
-        if (dp[i][j] != -1) return dp[i][j];
-        bool x = false;
-        bool y = false;
-
-        if (i - 1 >= 0 and s1[i - 1] == s3[k - 1])
+        
+        if(dp[i][j]!=-1)
         {
-            x = solve(s1, s2, s3, i - 1, j, k - 1);
+            return dp[i][j];
         }
-        if (j - 1 >= 0 and s2[j - 1] == s3[k - 1])
+        
+        bool x=false;
+        bool y=false;
+        
+        if(i!=s1.length())
         {
-            y = solve(s1, s2, s3, i, j - 1, k - 1);
+            if(s1[i]==s3[k])
+            {
+                x=solve(s1,s2,s3,i+1,j,k+1);
+            }
         }
-
-        return dp[i][j] = x || y;
+        
+        if(j!=s2.length())
+        {
+            if(s2[j]==s3[k])
+            {
+                y=solve(s1,s2,s3,i,j+1,k+1);
+            }
+        }
+            
+        return dp[i][j]=x||y;
     }
-    bool isInterleave(string s1, string s2, string s3)
-    {
-
-        int i = s1.length();
-        int j = s2.length();
-        int k = s3.length();
-        memset(dp, -1, sizeof dp);
-        return solve(s1, s2, s3, i, j, k);
+    bool isInterleave(string s1, string s2, string s3) {
+        
+        //base case
+        if(s1.length()+s2.length()!=s3.length())
+        {
+            return false;
+        }
+        memset(dp,-1,sizeof dp);
+        int i=0;
+        int j=0;
+        int k=0;
+        
+        
+        return solve(s1,s2,s3,i,j,k);
     }
 };
