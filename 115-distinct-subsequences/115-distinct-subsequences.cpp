7963c19b3d46985a1bede431f75ce7cd7ad5c82a
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-07-21 20:56:45 +0530

    Time: 40 ms (77.96%), Space: 18.6 MB (47.60%) - LeetHub

---
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -0,0 +1,35 @@
+class Solution
+{
+    public:
+        int solve(int i,int j,string &s,string &t,vector<vector<int>>&dp)
+        {
+            //base case
+            if(j<0)return 1;
+            if(i<0)return 0;
+            
+            if(dp[i][j]!=-1)return dp[i][j];
+            
+            
+            //choices diagram
+            
+            if(s[i]==t[j])
+            {
+                return dp[i][j]=solve(i-1,j-1,s,t,dp)+solve(i-1,j,s,t,dp);
+            }
+            else
+            {
+                return dp[i][j]=solve(i-1,j,s,t,dp);
+            }
+        }
+        int numDistinct(string s, string t)
+        {
+
+            int n = s.length();
+            int m = t.length();
+            vector<vector<int>>dp(n,vector<int>(m,-1));
+            
+            int i=n-1;
+            int j=m-1;
+            return solve(i,j,s,t,dp);
+        }
+};
