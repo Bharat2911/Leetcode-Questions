fa6e4f2af6d3d072e9ee28d35249d8baf883558e
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-07-29 00:19:04 +0530

    Time: 14 ms (57.46%), Space: 7.3 MB (45.59%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -2,22 +2,29 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         
-        //two freq array if both are same then return true else return fasle
+        //make the freq arr both the strings if its same then return true else false
+        int n=s.length();
+        int m=t.length();
         
         vector<int>for_s(26,0);
         vector<int>for_p(26,0);
         
-        if(s.length()!=t.length())return false;
-        
-        
-        for(int i=0;i<s.length();i++)
+        for(int i=0;i<n;i++)
         {
             for_s[s[i]-'a']++;
-            for_p[t[i]-'a']++;
         }
         
+        for(int j=0;j<m;j++)
+        {
+            for_p[t[j]-'a']++;
+        }
         
-        if(for_s!=for_p)return false;
-        return true;
+        if(for_s==for_p)
+        {
+            return true;
+        }
+        
+        return false;
+        
     }
 };
