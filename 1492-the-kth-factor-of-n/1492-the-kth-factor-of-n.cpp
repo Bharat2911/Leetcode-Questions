54965165fc5a8c3713038ff860ce0ded6e94005e
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-07-30 19:18:35 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (97.20%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.cpp
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.cpp
@@ -1,21 +1,23 @@
-class Solution {
-public:
-    int kthFactor(int n, int k) {
-        
-        vector<int>ans;
-        
-        for(int i=1;i<=n;i++)
+class Solution
+{
+    public:
+
+        int kthFactor(int n, int k)
         {
-            if(n%i==0)
+            int count=0;
+            
+
+            for (int i = 1; i <= n; i++)
             {
-                ans.push_back(i);
+                if (n % i == 0) 
+                {
+                    count++;
+                }
+                if(count==k)
+                {
+                    return i;
+                }
             }
+            return -1;
         }
-        sort(ans.begin(),ans.end());
-        
-        if(k>ans.size())return -1;
-        
-        return ans[k-1];
-        
-    }
 };
