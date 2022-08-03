1fc5a6726435c8be5fe14f053576b1eae3a57d56
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-08-03 12:10:42 +0530

    Time: 202 ms (34.25%), Space: 37.6 MB (87.88%) - LeetHub

---
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -0,0 +1,27 @@
+class MyCalendar {
+public:
+   
+    vector<pair<int,int>>v;
+    
+    MyCalendar() {
+        
+    }
+    
+    bool book(int start, int end) {
+        
+       for(auto it:v)
+        {
+		//NOT of (non overlapping  condition)
+            if(!(it.second<start or end-1<it.first))
+                return false;
+        }
+        v.push_back({start,end-1});
+        return true;
+    }
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
