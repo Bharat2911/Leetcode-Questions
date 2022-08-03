b22d62c625f35e58ac8e3f913bcce47af0a75ab6
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-08-03 14:01:05 +0530

    Time: 961 ms (53.05%), Space: 154.5 MB (35.33%) - LeetHub

--- 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.cpp
+++ 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.cpp
@@ -1,6 +1,7 @@
-class Solution {
-public:
-    void dfs(int start,unordered_map<int,bool>&vis,unordered_map<int,vector<int>>&adj,vector<int>&ans)
+class Solution
+{
+    public:
+    void dfs(int start,unordered_map<int,vector<int>>&adj,unordered_map<int,bool>&vis,vector<int>&ans)
     {
         vis[start]=true;
         
@@ -10,44 +11,39 @@ public:
         {
             if(!vis[itr])
             {
-                dfs(itr,vis,adj,ans);
+                dfs(itr,adj,vis,ans);
             }
         }
     }
-    vector<int> restoreArray(vector<vector<int>>& edges) {
-        
-        int n=edges.size();
-        
-        unordered_map<int,bool>vis;
-        
-        vector<int>ans;
-        
-        unordered_map<int,vector<int>>adj;
-        
-        for(int i=0;i<n;i++)
+        vector<int> restoreArray(vector<vector < int>> &edges)
         {
-            int u=edges[i][0];
-            int v=edges[i][1];
-            
-            adj[u].push_back(v);
-            adj[v].push_back(u);
-            
-        }
-        
-        //start node vo hoga jo one time appear ho rha hai adj list me
-        
-        for(auto itr:adj)
-        {
-            if(itr.second.size()==1)
+            int n = edges.size();
+
+            unordered_map<int, bool> vis;
+
+            unordered_map<int, vector < int>> adj;
+
+            for (int i = 0; i < n; i++)
             {
-                int start=itr.first;
-                
-                dfs(start,vis,adj,ans);
-                
-                break;
+                int u = edges[i][0];
+                int v = edges[i][1];
+
+                adj[u].push_back(v);
+                adj[v].push_back(u);
             }
+
+            vector<int> ans;
+
+            for (auto itr: adj)
+
+            {
+                if (itr.second.size() == 1)
+                {
+                    int start = itr.first;
+                    dfs(start, adj, vis, ans);
+                    break;
+                }
+            }
+            return ans;
         }
-        
-        return ans;
-    }
 };
