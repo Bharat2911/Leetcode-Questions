088bc481105fd3c2406db65d760ac08569564fe0
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-08-04 16:54:59 +0530

    Time: 91 ms (70.26%), Space: 21.4 MB (49.31%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -1,76 +1,67 @@
-class Solution
-{
-    public:
-        int maxDistance(vector<vector < int>> &grid)
+class Solution {
+public:
+    int maxDistance(vector<vector<int>>& mat) {
+        
+        //we have to find the distance of 1 from all cell and return the max distance
+          int row=mat.size();
+        int col=mat[0].size();
+        
+        vector<vector<int>>ans(row,vector<int>(col,INT_MAX));
+        
+        //queue for bfs
+        int count_zero=0;
+        
+        queue<pair<int,int>>q;
+        
+        for(int i=0;i<row;i++)
         {
-
-           	//this is the multisoure bfs
-            int row = grid.size();
-            int col = grid[0].size();
-
-           	//making an anser matrux
-            vector<vector < int>> ans(row, vector<int> (col, INT_MAX));	//initially filled with infinity
-
-            queue<pair<int, int>> q;
-
-           	//push all the coordinates of one in quwuw
-
-            for (int i = 0; i < row; i++)
+            for(int j=0;j<col;j++)
             {
-                for (int j = 0; j < col; j++)
+                if(mat[i][j]==1)
                 {
-                    if (grid[i][j] == 1)
-                    {
-                        q.push({ i,
-                            j });
-                        ans[i][j] = 0;	//since distance of 1 from 1 is 0(everytime)
-                    }
+                    ans[i][j]=0;
+                    //multi source bfs
+                    q.push({i,j});
+                }
+                else
+                {
+                    count_zero++;
                 }
             }
-
-            int dx[] = { 0,
-                1,
-                0,
-                -1
-            };
-
-            int dy[] = { 1,
-                0,
-                -1,
-                0
-            };
-            int dist=0;
-            
-
-            while (!q.empty())
-            {
-                int sz = q.size();
-                dist++;
-
-                while (sz--)
-                {
-                    int i = q.front().first;
-                    int j = q.front().second;
-                    q.pop();
-
-                    for (int idx = 0; idx < 4; idx++)
-                    {
-                        int newi = i + dx[idx];
-                        int newj = j + dy[idx];
-
-                        if (newi >= 0 and newi < row and newj >= 0 and newj < col and ans[i][j] + 1 < ans[newi][newj])
-                        {
-                            ans[newi][newj] = ans[i][j] + 1;
-                            q.push({ newi,
-                                newj });
-                        }
-                    }
-                }
-               	//            
-            }
-           	//now find the max in ans matrix
-            
-            
-            return dist>1?dist-1:-1;
         }
+        if(count_zero==0)return -1;
+        
+        int dx[4]={0,1,0,-1};
+        int dy[4]={1,0,-1,0};
+        
+        while(!q.empty())
+        {
+            int i=q.front().first;
+            int j=q.front().second;
+            q.pop();
+            
+            for(int idx=0;idx<4;idx++)
+            {
+                int newi=i+dx[idx];
+                int newj=j+dy[idx];
+                
+                
+                if(newi>=0 and newi<row and newj>=0 and newj<col and ans[i][j]+1<ans[newi][newj])
+                {
+                    ans[newi][newj]=1+ans[i][j];
+                    q.push({newi,newj});
+                }
+            }
+        }
+        int ma=0;
+        for(int i=0;i<row;i++)
+        {
+            for(int j=0;j<col;j++)
+            {
+                ma=max(ma,ans[i][j]);
+            }
+        }
+        if(ma==INT_MAX)return -1;
+        return ma;
+    }
 };
