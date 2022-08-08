d225a58d848c4ef020c87e6839020cee1ffb6cd6
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-08-08 09:15:27 +0530

    Time: 20 ms (17.42%), Space: 17.1 MB (11.07%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,48 +1,41 @@
-class Solution
-{
-    public:
-        int solve(int idx, int n, vector<int> &prices, bool isbuy,vector<vector<int>>&dp)
-        {
-           	//base case
-            if (idx == n)
-            {
-                return 0;
-            }
-            
-            if(dp[idx][isbuy]!=-1)
-            {
-                return dp[idx][isbuy];
-            }
-           	//choice didagram
-
-           	//if buy allowed 
-           	// -> buy
-           	// ->not buy
-
-           	//if sell allowed
-           	// ->sell
-           	// ->not sell
-            int profit = 0;
-
-            if (isbuy)
-            {
-                profit = max(-prices[idx] + solve(idx + 1, n, prices, false,dp), 0 + solve(idx + 1, n, prices, true,dp));
-            }
-            else
-            {
-               	//sell is alowed
-                profit = max(prices[idx] + solve(idx + 1, n, prices, true,dp), 0 + solve(idx + 1, n, prices, false,dp));
-            }
-
-            return dp[idx][isbuy]=profit;
-        }
-    int maxProfit(vector<int> &prices)
+class Solution {
+public:
+    int solve(int idx,vector<int>&prices,bool is_buy,vector<vector<int>>&dp)
     {
-
-        int idx = 0;
-        int n = prices.size();
-        bool isbuy = true;
-        vector<vector < int>> dp(n, vector<int> (2, -1));
-        return solve(idx, n, prices, isbuy,dp);
+        //base case
+        if(idx==prices.size())
+        {
+            return 0;
+        }
+        
+        int profit=0;
+        
+        if(dp[idx][is_buy]!=-1)return dp[idx][is_buy];
+        
+        if(is_buy==true)
+        {
+            profit=max(-prices[idx]+solve(idx+1,prices,false,dp),solve(idx+1,prices,true,dp));
+        }
+        
+        else
+        {
+            profit=max(prices[idx]+solve(idx+1,prices,true,dp),solve(idx+1,prices,false,dp));
+        }
+        return dp[idx][is_buy]=profit;
+    }
+    int maxProfit(vector<int>& prices) {
+        
+        //we will be doing this using recursion bitch
+        int n=prices.size();
+        
+        vector<vector<int>>dp(n+1,vector<int>(3,-1));
+        
+        int idx=0;
+        
+        bool is_buy=true;
+        
+        return solve(idx,prices,is_buy,dp);
+        
+        
     }
 };
