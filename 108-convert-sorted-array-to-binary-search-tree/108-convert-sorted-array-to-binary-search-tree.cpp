ae40856d769a9bb1b8f0613bc25dc723fd3b081a
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-08-10 09:38:17 +0530

    Time: 23 ms (52.91%), Space: 21.4 MB (41.16%) - LeetHub

--- 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.cpp
+++ 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.cpp
@@ -13,19 +13,14 @@ class Solution {
 public:
     TreeNode*solve(vector<int>&nums,int lb,int ub)
     {
-        //base case
-        if(lb>ub)
-        {
-            return NULL;
-        }
+        if(lb>ub)return NULL;
         
-        int index=(ub+lb)/2;
+        int idx=(lb+ub)/2;
         
-        TreeNode*root= new TreeNode(nums[index]);
+        TreeNode*root=new TreeNode (nums[idx]);
         
-        root->left=solve(nums,lb,index-1);
-        
-        root->right=solve(nums,index+1,ub);
+        root->left=solve(nums,lb,idx-1);
+        root->right=solve(nums,idx+1,ub);
         
         return root;
     }
@@ -34,10 +29,8 @@ public:
         int n=nums.size();
         
         int lb=0;
-        
         int ub=n-1;
         
         return solve(nums,lb,ub);
-        
     }
 };
