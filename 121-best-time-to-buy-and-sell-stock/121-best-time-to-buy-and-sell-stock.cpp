ece53fe8f381e6f4906783bb021d136a8968e237
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-08-12 19:37:29 +0530

    Time: 166 ms (75.13%), Space: 93.3 MB (89.63%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -5,16 +5,13 @@ public:
         int n=prices.size();
         
         int mini=prices[0];
-        
-        int profit=INT_MIN;
+        int profit=0;
         
         for(int i=0;i<n;i++)
         {
             mini=min(mini,prices[i]);
-            
             profit=max(profit,prices[i]-mini);
         }
-        
         return profit;
     }
 };
