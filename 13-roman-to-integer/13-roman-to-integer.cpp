45db91d1bf5b6bc3c5d1ef986b7ede87187a0931
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-08-15 18:48:31 +0530

    Time: 23 ms (39.99%), Space: 8 MB (37.92%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -1,41 +1,32 @@
-class Solution
-{
-    public:
-        int romanToInt(string s)
+class Solution {
+public:
+    int romanToInt(string s) {
+        
+        unordered_map<char,int>map;
+            
+            int n=s.length();
+        
+            map.insert({'I',1});
+         map.insert({'V',5});
+         map.insert({'X',10});
+         map.insert({'L',50});
+         map.insert({'C',100});
+         map.insert({'D',500});
+         map.insert({'M',1000});
+        
+        int ans=map[s[n-1]];
+        
+        for(int i=n-2;i>=0;i--)
         {
-
-            unordered_map<char, int> map;
-
-            map.insert({ 'I',
-                1 });
-            map.insert({ 'V',
-                5 });
-            map.insert({ 'X',
-                10 });
-            map.insert({ 'L',
-                50 });
-            map.insert({ 'C',
-                100 });
-            map.insert({ 'D',
-                500 });
-            map.insert({ 'M',
-                1000 });
-
-           	//itrate from reverse
-            int n = s.length();
-            int sum = 0;
-
-            for (int i =0; i <n; i++)
+            if(map[s[i]]<map[s[i+1]])
             {
-                if (map[s[i]] < map[s[i + 1]])
-                {
-                    sum += map[s[i + 1]] - map[s[i]];
-                    i++;
-                    continue;
-                }
-                sum += map[s[i]];
+                ans-=map[s[i]];
             }
-
-            return sum;
+            else
+            {
+                ans+=map[s[i]];
+            }
         }
+        return ans;
+    }
 };
