26bf28648fe752c0d6954feeb481ed5545dad53c
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-08-17 12:35:35 +0530

    Time: 10 ms (20.67%), Space: 8.6 MB (30.73%) - LeetHub

--- 804-unique-morse-code-words/804-unique-morse-code-words.cpp
+++ 804-unique-morse-code-words/804-unique-morse-code-words.cpp
@@ -2,34 +2,36 @@ class Solution {
 public:
     int uniqueMorseRepresentations(vector<string>& words) {
         
-        unordered_map<char,string>map;
+        // unordered_map<char,string>map;
         
-        map.insert({'a',".-"});
-        map.insert({'b',"-..."});
-        map.insert({'c',"-.-."});
-        map.insert({'d',"-.."});
-        map.insert({'e',"."});
-        map.insert({'f',"..-."});
-        map.insert({'g',"--."});
-        map.insert({'h',"...."});
-        map.insert({'i',".."});
-        map.insert({'j',".---"});
-        map.insert({'k',"-.-"});
-        map.insert({'l',".-.."});
-        map.insert({'m',"--"});
-        map.insert({'n',"-."});
-        map.insert({'o',"---"});
-        map.insert({'p',".--."});
-        map.insert({'q',"--.-"});
-        map.insert({'r',".-."});
-        map.insert({'s',"..."});
-        map.insert({'t',"-"});
-        map.insert({'u',"..-"});
-        map.insert({'v',"...-"});
-        map.insert({'w',".--"});
-        map.insert({'x',"-..-"});
-        map.insert({'y',"-.--"});
-        map.insert({'z',"--.."});
+//         map.insert({'a',".-"});
+//         map.insert({'b',"-..."});
+//         map.insert({'c',"-.-."});
+//         map.insert({'d',"-.."});
+//         map.insert({'e',"."});
+//         map.insert({'f',"..-."});
+//         map.insert({'g',"--."});
+//         map.insert({'h',"...."});
+//         map.insert({'i',".."});
+//         map.insert({'j',".---"});
+//         map.insert({'k',"-.-"});
+//         map.insert({'l',".-.."});
+//         map.insert({'m',"--"});
+//         map.insert({'n',"-."});
+//         map.insert({'o',"---"});
+//         map.insert({'p',".--."});
+//         map.insert({'q',"--.-"});
+//         map.insert({'r',".-."});
+//         map.insert({'s',"..."});
+//         map.insert({'t',"-"});
+//         map.insert({'u',"..-"});
+//         map.insert({'v',"...-"});
+//         map.insert({'w',".--"});
+//         map.insert({'x',"-..-"});
+//         map.insert({'y',"-.--"});
+//         map.insert({'z',"--.."});
+        
+        vector<string>map={".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
         
         set<string>set;
         
@@ -39,7 +41,7 @@ public:
             
             for(int i=0;i<itr.length();i++)
             {
-                str+=map[itr[i]];
+                str+=map[itr[i]-'a'];
             }
             set.insert(str);
         }
