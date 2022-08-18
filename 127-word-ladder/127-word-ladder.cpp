4b7ebd547680f19a722a898de7901c3157123b68
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-08-18 13:39:57 +0530

    Time: 966 ms (33.23%), Space: 28.1 MB (20.45%) - LeetHub

--- 127-word-ladder/127-word-ladder.cpp
+++ 127-word-ladder/127-word-ladder.cpp
@@ -1,84 +1,77 @@
-class Solution
-{
-    public:
-    bool differ_by_one(string &str,string &ptr)
+class Solution {
+public:
+    bool is_diff(string &a,string&b)
     {
         int count=0;
         
-        for(int i=0;i<str.length();i++)
+        for(int i=0;i<a.length();i++)
         {
-            if(str[i]!=ptr[i])
+            if(a[i]!=b[i])
             {
                 count++;
+                
             }
         }
         if(count==1)return true;
-        return false;
+        else return false;
         
     }
-        int ladderLength(string beginWord, string endWord, vector<string> &wordList)
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        
+        unordered_map<string,vector<string>>adj;
+        
+        //basic approach of mine is to do bfs
+        
+        int n=wordList.size();
+        
+        for(int i=0;i<n;i++)
         {
-
-           	//i want to make the adj list and i will make in using the map
-           map<string, vector < string>> adj;
-
-           	//will itrate in the word list and fill the map
-            int n = wordList.size();
-
-            for (int i = 0; i < n; i++)
+            for(int j=i+1;j<n;j++)
             {
-                for (int j = i + 1; j < n; j++)
+                if(is_diff(wordList[i],wordList[j]))
                 {
-                    if (differ_by_one(wordList[i], wordList[j]))
-                    {
-                        adj[wordList[i]].push_back(wordList[j]);
-                        adj[wordList[j]].push_back(wordList[i]);
-                    }
+                    adj[wordList[i]].push_back(wordList[j]);
+                    adj[wordList[j]].push_back(wordList[i]);
                 }
             }
-           	//we also have to add beginWord into our adj list
-            for (int i = 0; i < n; i++)
-            {
-                if (differ_by_one(beginWord, wordList[i]))
-                {
-                    adj[beginWord].push_back(wordList[i]);
-                    adj[wordList[i]].push_back(beginWord);
-                }
-            }
-
-           	//now we will do the bsf and find the min distance between beginWord and endWord
-
-            set<string> vis;
-
-            queue<pair<string, int>> q;
-
-            q.push({ beginWord,
-                1 });
-
-            vis.insert(beginWord);
-
-            while (!q.empty())
-            {
-                string str = q.front().first;
-                int level = q.front().second;
-                q.pop();
-
-               	//itrate in the adj list
-                for (auto itr: adj[str])
-                {
-                   	// adjacent node is not present in visited vector then we can move further
-                    if (vis.find(itr) == vis.end())
-                    {
-                        vis.insert(itr);
-                        
-                        q.push({itr,level + 1});
-                    }
-                    if(itr==endWord)
-                    {
-                        return level+1;
-                    }
-                }
-            }
-            return 0;
         }
+        for(int i=0;i<n;i++)
+        {
+            if(is_diff(beginWord,wordList[i]))
+            {
+                adj[beginWord].push_back(wordList[i]);
+                adj[wordList[i]].push_back(beginWord);
+            }
+        }
+        
+        set<string>vis;
+        
+        queue<pair<string,int>>q;
+        
+        q.push({beginWord,1});
+        
+        vis.insert(beginWord);
+        
+        while(!q.empty())
+        {
+            string word=q.front().first;
+            int level=q.front().second;
+            
+            q.pop();
+            
+            for(auto itr:adj[word])
+            {
+                if(vis.find(itr)==vis.end())
+                {
+                    vis.insert(itr);
+                    q.push({itr,level+1});
+                }
+                if(itr==endWord)
+                {
+                    return level+1;
+                }
+            }
+        }
+        return 0;
+    }
 };
