d7bb2fd7ff133e8f1365b1f0bdc9b3fc97fb3a54
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-08-24 15:26:59 +0530

    Time: 143 ms (25.45%), Space: 47.4 MB (9.41%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,44 +1,25 @@
 class Solution {
 public:
+    //T.C=o(n)
+    //S.C=o(k)//onlu k size pq i am using na
     int findKthLargest(vector<int>& nums, int k) {
         
-        //brute force sort and find
+        //using the priority_queue to solve this
         
-        sort(nums.begin(),nums.end());
-        reverse(nums.begin(),nums.end());
+        //making the min heap
         
-        return nums[k-1];
+        priority_queue<int,vector<int>,greater<int>>pq;
         
-        // T.c=O(N log n)
-        // S.C =O(1);
-        
-        
-        
-        //optimised using the heap
-        
-        //max heap
-        priority_queue<int>pq;
         
         for(auto itr:nums)
         {
             pq.push(itr);
-        }
-        
-        k--;
-        
-        while(!pq.empty() and k>0)
-        {
-            pq.pop();
             
-            k--;
+            if(pq.size()>k)
+            {
+                pq.pop();
+            }
         }
-        
         return pq.top();
-        
-        
-        //T.c ==O(n log k)
-        //S.C ==o(n);
-        
-        
     }
 };
