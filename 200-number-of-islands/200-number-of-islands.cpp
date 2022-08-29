4b44d0ce925ff29050c21deb5040f9e236a523de
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-08-29 15:17:12 +0530

    Time: 71 ms (27.93%), Space: 19.3 MB (9.36%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,65 +1,52 @@
-class Solution
-{
-    public:
-        void solve(int i, int j, vector<vector < char>> &grid, int row, int col, vector< vector< bool>> &vis)
+class Solution {
+public:
+    void solve(int i,int j,vector<vector<char>>&grid,vector<vector<int>>&vis,int row,int col)
+    {
+        queue<pair<int,int>>q;
+        
+        vis[i][j]=1;
+        q.push({i,j});
+        
+        int dx[4]={0,1,0,-1};
+        int dy[4]={1,0,-1,0};
+        
+        while(!q.empty())
         {
-            int dx[4] = { 0,
-                1,
-                0,
-                -1
-            };
-
-            int dy[4] = { 1,
-                0,
-                -1,
-                0
-            };
-
-            queue<pair<int, int>> q;
-
-            vis[i][j] = true;
-            q.push({ i,
-                j });
-
-            while (!q.empty())
+            int x=q.front().first;
+            int y=q.front().second;
+            
+            q.pop();
+            
+            for(int idx=0;idx<4;idx++)
             {
-                int x=q.front().first;
-                int y=q.front().second;
-                q.pop();
+                int newi=x+dx[idx];
+                int newj=y+dy[idx];
                 
-                for (int idx = 0; idx < 4; idx++)
+                if(newi>=0 and newi<row and newj>=0 and newj<col and !vis[newi][newj] and grid[newi][newj]=='1')
                 {
-                    int newi = x + dx[idx];
-                    int newj = y + dy[idx];
-
-                    if (newi >= 0 and newi < row and newj >= 0 and newj < col and!vis[newi][newj] and grid[newi][newj] == '1')
-                    {
-                        vis[newi][newj] = true;
-
-                        q.push({ newi,
-                            newj });
-                    }
+                    vis[newi][newj]=1;
+                    q.push({newi,newj});
                 }
             }
         }
-    int numIslands(vector<vector < char>> &grid)
-    {
-
-        int row = grid.size();
-        int col = grid[0].size();
-
-        int count = 0;
-
-        vector<vector < bool>> vis(row, vector<bool> (col, false));
-
-        for (int i = 0; i < row; i++)
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        
+        int row=grid.size();
+        int col=grid[0].size();
+        
+        int count=0;
+        
+        vector<vector<int>>vis(row,vector<int>(col,0));
+        
+        for(int i=0;i<row;i++)
         {
-            for (int j = 0; j < col; j++)
+            for(int j=0;j<col;j++)
             {
-                if (!vis[i][j] and grid[i][j] == '1')
+                if(!vis[i][j] and grid[i][j]=='1')
                 {
                     count++;
-                    solve(i, j, grid, row, col, vis);
+                    solve(i,j,grid,vis,row,col);
                 }
             }
         }
