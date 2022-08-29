fb9debddac0525e5cd4d623ac13fb56e81a2ae58
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-08-29 15:51:27 +0530

    Time: 137 ms (57.15%), Space: 50.3 MB (8.26%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
-  
-    int solve(int idx,int n,vector<int>&coins,int sum,vector<vector<int>>&dp)
+    
+    int solve(int idx,vector<int>&coins,int sum,int n,vector<vector<int>>&dp)
     {
-        if(idx==n)return 1e9;
+        if(idx==n)
+        {
+            return 1e9;
+        }
         
-        if(sum==0)return 1;
+        if(sum==0)
+        {
+            return 1;
+        }
         
         if(dp[idx][sum]!=-1)return dp[idx][sum];
         
         if(coins[idx]<=sum)
         {
-            return dp[idx][sum]=min(1+solve(idx,n,coins,sum-coins[idx],dp),solve(idx+1,n,coins,sum,dp));
+            return dp[idx][sum]=min(1+solve(idx,coins,sum-coins[idx],n,dp),solve(idx+1,coins,sum,n,dp));
         }
         else
         {
-            return dp[idx][sum]=solve(idx+1,n,coins,sum,dp);
+            return dp[idx][sum]=solve(idx+1,coins,sum,n,dp);
         }
     }
     int coinChange(vector<int>& coins, int amount) {
@@ -28,6 +34,12 @@ public:
         
         vector<vector<int>>dp(n+1,vector<int>(sum+1,-1));
         
-        return solve(idx,n,coins,sum,dp)>=1e9?-1:solve(idx,n,coins,sum,dp)-1;
+        if(solve(idx,coins,sum,n,dp)>=1e9)
+        {
+            return -1;
+        }
+        
+        else return solve(idx,coins,sum,n,dp)-1;
+        
     }
 };
