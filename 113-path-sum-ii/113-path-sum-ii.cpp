1c9d3f08d774b49e5842ca7f4815c6a1d67343b0
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-09-01 14:00:10 +0530

    Time: 42 ms (12.76%), Space: 38.4 MB (26.63%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.cpp
+++ 113-path-sum-ii/113-path-sum-ii.cpp
@@ -1,60 +1,61 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *int val;
+ *TreeNode * left;
+ *TreeNode * right;
+ *TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
-    void solve(TreeNode*root,int tsum,vector<vector<int>>&res,vector<int>&ans,int sum)
-    {
-        if(root==NULL)
+class Solution
+{
+    public:
+        void solve(TreeNode *root, vector<vector < int>> &ans, vector< int > res)
         {
-            return;
-        }
-        ans.push_back(root->val);
-        
-        if(root->left==NULL and root->right==NULL)//leaf node encounter
-        {
-            sum+=root->val;
-            
-            if(sum==tsum)
+            if (root == NULL) return;
+
+            if (root->left == NULL and root->right == NULL)
             {
-                res.push_back(ans);
+                res.push_back(root->val);
+
+                ans.push_back(res);
+
+                return;
             }
-            
+            res.push_back(root->val);
+
+            solve(root->left, ans, res);
+
+            solve(root->right, ans, res);
         }
-        sum+=root->val;
-        
-        solve(root->left,tsum,res,ans,sum);
-        
-        solve(root->right,tsum,res,ans,sum);
-        
-        //in the end just add a backtraking step
-        
-        ans.pop_back();//backtraking
-        
-        
-    }
-    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
-        
-        vector<vector<int>>res;
-        
-        vector<int>ans;
-        
-        if(root==NULL)
+    vector<vector < int>> pathSum(TreeNode *root, int targetSum)
+    {
+
+        vector<vector < int>> res;
+
+        vector<int> ans;
+
+        int sum = 0;
+
+        solve(root, res, ans);
+
+        vector<vector < int>> v;
+
+        for (auto itr: res)
         {
-            return res;
+            int sum = 0;
+
+            for (auto x: itr)
+            {
+                sum += x;
+            }
+            if (sum == targetSum)
+            {
+                v.push_back(itr);
+            }
         }
-        int sum=0;
-        
-        solve(root,targetSum,res,ans,sum);
-        
-        return res;
+        return v;
     }
 };
