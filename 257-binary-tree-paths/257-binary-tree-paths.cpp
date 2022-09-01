5ffbe67e1c3a3ef1baedf142d00f9c1c306abae2
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-09-01 10:45:46 +0530

    Time: 3 ms (89.00%), Space: 13 MB (62.54%) - LeetHub

--- 257-binary-tree-paths/257-binary-tree-paths.cpp
+++ 257-binary-tree-paths/257-binary-tree-paths.cpp
@@ -1,48 +1,50 @@
 /**
- *Definition for a binary tree node.
- *struct TreeNode {
- *  int val;
- *  TreeNode * left;
- *  TreeNode * right;
- *  TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- *};
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
  */
-class Solution
-{
-    public:
-        void solve(TreeNode *root, vector<string> &ans, string str)
+class Solution {
+public:
+    void solve(TreeNode*root,vector<string>&ans,string res)
+    {
+        if(root==NULL)return;
+        
+        if(root->left==NULL and root->right==NULL)
         {
-            if(root==NULL)
-            {
-                return;
-            }
+            res+=to_string(root->val);
             
-            if(root->left==NULL and root->right==NULL)
-            {
-                str+=to_string(root->val);
-                
-                ans.push_back(str);
-                return;
-            }
+            ans.push_back(res);
             
-            str+=(to_string(root->val)+"->");
-            
-            solve(root->left,ans,str);
-            
-            solve(root->right,ans,str);
+            return;
             
         }
-    vector<string> binaryTreePaths(TreeNode *root)
-    {
-
-        vector<string> ans;
-
-        string str = "";
-
-        solve(root, ans, str);
-
+        
+        res+=to_string(root->val)+"->";
+        
+        solve(root->left,ans,res);
+        solve(root->right,ans,res);
+        
+    }
+    vector<string> binaryTreePaths(TreeNode* root) {
+        
+        vector<string>ans;
+        
+        if(root==NULL)
+        {
+            return ans;
+        }
+        
+        string res="";
+        
+        solve(root,ans,res);
+        
         return ans;
+        
     }
 };
