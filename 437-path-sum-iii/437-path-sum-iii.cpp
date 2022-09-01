100aa528aacb8237883c8e121b0a599fe3f5d534
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-09-01 14:20:18 +0530

    Time: 52 ms (28.34%), Space: 15.9 MB (79.04%) - LeetHub

--- 437-path-sum-iii/437-path-sum-iii.cpp
+++ 437-path-sum-iii/437-path-sum-iii.cpp
@@ -1,36 +1,37 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
-    int count=0;
-    void solve(TreeNode*root,int ts,long long sum)
+class Solution
+{
+    public:
+        int ans = 0;
+
+    void solve(TreeNode *root, int targetSum, long long sum)
     {
-        if(root==NULL)
+        if (root == NULL) return;
+
+        sum += root->val;
+
+        if (sum == targetSum)
         {
-            return;
+            ans++;
         }
-        
-        
-        
-        sum+=root->val;
-        
-        if(sum==ts)count++;
-        
-        solve(root->left,ts,sum);
-        solve(root->right,ts,sum);
-        
+
+        solve(root->left, targetSum, sum);
+
+        solve(root->right, targetSum, sum);
     }
-    int pathSum(TreeNode* root, int targetSum) {
-        
+    int pathSum(TreeNode *root, int targetSum)
+    {
+
         if(root==NULL)
         {
             return 0;
@@ -43,6 +44,6 @@ public:
         pathSum(root->left,targetSum);
         pathSum(root->right,targetSum);
         
-        return count;
+        return ans;
     }
 };
