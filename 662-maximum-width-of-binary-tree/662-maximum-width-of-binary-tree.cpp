468a354ef82c7520841879df8db2c470826b410a
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-09-01 09:39:32 +0530

    Time: 15 ms (53.40%), Space: 17.3 MB (55.29%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -1,62 +1,51 @@
 /**
- *Definition for a binary tree node.
- *struct TreeNode {
- *    int val;
- *    TreeNode * left;
- *    TreeNode * right;
- *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- *};
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
  */
-class Solution
-{
-    public:
-        int widthOfBinaryTree(TreeNode *root)
+class Solution {
+public:
+    int widthOfBinaryTree(TreeNode* root) {
+        
+        if(root==NULL)
         {
-
-           	//we will be using the concept of indexing here
-            if (root == NULL)
+            return 0;
+        }
+        queue<pair<TreeNode*,int>>q;
+        
+        q.push({root,0});
+        
+        int ans=0;
+        
+        while(!q.empty())
+        {
+            int sz=q.size();
+            
+            ans=max(ans,q.back().second-q.front().second+1);
+            
+            while(sz--)
             {
-                return 0;
-            }
-            queue<pair<TreeNode*, int>> q;
-
-            int ans = 0;
-
-            q.push({ root,
-                0 });
-
-            while (!q.empty())
-            {
-
-               	//ans right_most_index - left_most_index +1
-
-                int right_idx = q.back().second;
-                int left_idx = q.front().second;
-
-                ans = max(ans, right_idx - left_idx + 1);
-
-                int sz = q.size();
-
-                while (sz--)
+                TreeNode*temp=q.front().first;
+                int idx=q.front().second;
+                q.pop();
+                
+                if(temp->left)
                 {
-                    TreeNode *temp = q.front().first;
-                    int index = q.front().second;
-                    q.pop();
-
-                    if (temp->left)
-                    {
-                        q.push({ temp->left,
-                           (long long) 2 *index + 1 });
-                    }
-                    if (temp->right)
-                    {
-                        q.push({ temp->right,
-                          (long long)  2 *index + 2 });
-                    }
+                    q.push({temp->left,(long long)2*idx+1});
+                }
+                
+                if(temp->right)
+                {
+                    q.push({temp->right,(long long)2*idx+2});
                 }
             }
-            return ans;
         }
+        return ans;
+    }
 };
