2fb051e6df201425d250e9f52a4f71face244d4b
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-09-01 13:44:44 +0530

    Time: 24 ms (21.96%), Space: 21.4 MB (39.10%) - LeetHub

--- 112-path-sum/112-path-sum.cpp
+++ 112-path-sum/112-path-sum.cpp
@@ -1,53 +1,46 @@
 /**
  *Definition for a binary tree node.
  *struct TreeNode {
- *    int val;
- *    TreeNode * left;
- *    TreeNode * right;
- *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *   int val;
+ *   TreeNode * left;
+ *   TreeNode * right;
+ *   TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  *};
  */
 class Solution
 {
     public:
-        bool f = false;
-    void solve(TreeNode *root, int tsum, int sum)
+        bool ans;
+
+    void solve(TreeNode *root, int sum, int &target)
     {
-        if (root == NULL)
-        {
-            return ;
-        }
+        if (root == NULL) return;
+
+        sum += root->val;
 
         if (root->left == NULL and root->right == NULL)
         {
-            sum += root->val;
-
-            if (sum == tsum)
+            if (sum == target)
             {
-                f = true;
+                ans = true;
+            }
+            else
+            {
+                sum=0;
             }
         }
-        
-        sum+=root->val;
-        
-        solve(root->left,tsum,sum);
-        
-        solve(root->right,tsum,sum);
-        
+        solve(root->left, sum, target);
+        solve(root->right, sum, target);
     }
-    bool hasPathSum(TreeNode *root, int tsum)
+    bool hasPathSum(TreeNode *root, int targetSum)
     {
 
-        if (root == NULL)
-        {
-            return false;
-        }
-       int sum=0;
-        
-        solve(root, tsum,sum);
+        int sum = 0;
 
-        return f;
+        solve(root, sum, targetSum);
+
+        return ans;
     }
 };
