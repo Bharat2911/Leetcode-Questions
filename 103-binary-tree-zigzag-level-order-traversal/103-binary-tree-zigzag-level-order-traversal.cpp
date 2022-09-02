cb51f3e35efd6da5f5675b4202ca26b2331dccf6
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-09-02 14:19:23 +0530

    Time: 8 ms (26.91%), Space: 12 MB (75.57%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.cpp
@@ -1,64 +1,64 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
-    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        
-        vector<vector<int>>res;
-        
-        if(root==NULL)
+class Solution
+{
+    public:
+        vector<vector < int>> zigzagLevelOrder(TreeNode *root)
         {
-            return res;
-        }
-        
-        queue<TreeNode*>q;
-        q.push(root);
-        int count=-1;
-        
-        //i will increment the count in end so that it check for level 0 also
-        while(!q.empty())
-        {
-            int sz=q.size();
-            vector<int>ans;
-            count++;
-            while(sz--)
+
+            vector<vector < int>> ans;
+
+            if (root == NULL)
             {
-                TreeNode*temp=q.front();
-                q.pop();
-                
-                ans.push_back(temp->val);
-                
-                if(temp->left)
+                return ans;
+            }
+
+            int count = -1;
+
+            queue<TreeNode*> q;
+
+            q.push(root);
+
+            while (!q.empty())
+            {
+                int sz = q.size();
+
+                vector<int> res;
+
+                while (sz--)
                 {
-                    q.push(temp->left);
+                    TreeNode *temp = q.front();
+                    q.pop();
+
+                    res.push_back(temp->val);
+
+                    if (temp->left)
+                    {
+                        q.push(temp->left);
+                    }
+
+                    if (temp->right)
+                    {
+                        q.push(temp->right);
+                    }
                 }
-                
-                if(temp->right)
+
+                count++;
+                if (count & 1)
                 {
-                    q.push(temp->right);
+                    reverse(res.begin(), res.end());
                 }
+                ans.push_back(res);
             }
-            if(count&1)
-            {
-                reverse(ans.begin(),ans.end());
-                res.push_back(ans);
-            }
-            else
-            {
-                res.push_back(ans);
-            }
-            
-            // count++;
+            return ans;
         }
-        return res;
-    }
 };
