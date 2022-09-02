c1891b7dbbbdd6b3284d3bfb3924adebd0d5e47e
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-09-02 23:29:46 +0530

    Time: 11 ms (98.82%), Space: 26.4 MB (25.32%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,47 +1,52 @@
 /**
- *Definition for a binary tree node.
- *struct TreeNode {
- *    int val;
- *    TreeNode * left;
- *    TreeNode * right;
- *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- *};
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
  */
-class Solution
-{
-    public:
-        int i = 0;
-    unordered_map<int, int> map;
-
-    TreeNode* solve(vector<int> &preorder, vector<int> &inorder, int lb, int ub)
+class Solution {
+public:
+    unordered_map<int,int>map;
+    int idx=0;
+    
+    TreeNode*solve(vector<int>&pre,vector<int>&in,int lb,int ub)
     {
-        if (lb > ub) return NULL;
-
-        int index = map[preorder[i]];
-
-        TreeNode *root = new TreeNode(preorder[i]);
-        i++;
-
-        root->left = solve(preorder, inorder, lb, index - 1);
-        root->right = solve(preorder, inorder, index + 1, ub);
-
+        //base case
+        if(lb>ub)return NULL;
+        
+       
+        
+        TreeNode* root=new TreeNode(pre[idx]);
+        
+        int i=map[pre[idx]];
+        idx++;
+        
+        root->left=solve(pre,in,lb,i-1);
+        
+        root->right=solve(pre,in,i+1,ub);
+        
         return root;
     }
-    TreeNode* buildTree(vector<int> &preorder, vector<int> &inorder)
-    {
-
-       	//ele of inorder with therie index
-
-        for (int i = 0; i < inorder.size(); i++)
+    
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        
+        int n=inorder.size();
+        
+        int lb=0;
+        
+        int ub=n-1;
+        
+        for(int i=0;i<n;i++)
         {
-            map[inorder[i]] = i;
+            map[inorder[i]]=i;
+            
         }
-
-        int lb = 0;
-        int ub = inorder.size() - 1;
-
-        return solve(preorder, inorder, lb, ub);
+        
+        return solve(preorder,inorder,lb,ub);
     }
 };
