76d56b5bec52e1d90eed8416ce45dae3a2853237
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-09-10 00:48:43 +0530

    Time: 24 ms (80.96%), Space: 6.9 MB (84.61%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,46 +1,52 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-         int n=s.length();
-        int start=0;
-        int end=1;
         
-        for(int i=1;i<n;i++)
+        int n=s.length();
+        
+        int start=0;
+        
+        int maxi=1;
+        
+        for(int i=0;i<n;i++)
         {
+            
             //odd case
             int low=i-1;
-            int high=i;
-            while(low>=0 && high<n && s[low]==s[high])
-            {
-                if(high-low+1>end)
-                {
-                    start=low;
-                    end=high-low+1;
-                    
-                }
-                low--;
-                high++;
-            }
-            //even case
-               low=i-1;
-               high=i+1;
+            int high=i+1;
             
-            while(low>=0 && high<n && s[low]==s[high])
+            while(low>=0 and high<n and s[low]==s[high])
             {
-                if(high-low+1>end)
+                if(high-low+1>maxi)
                 {
                     start=low;
-                    end=high-low+1;
+                    maxi=high-low+1;
                 }
-                low--;
-                high++;
+                
+                //expand kar rha hu 
+                low-=1;
+                high+=1;
+                
+            }
+            
+            //even case
+            low=i;
+            high=i+1;
+            
+            while(low>=0 and high<n and s[low]==s[high])
+            {
+                if(high-low+1>maxi)
+                {
+                    start=low;
+                    maxi=high-low+1;
+                }
+                
+                //expand kar rha hu 
+                low-=1;
+                high+=1;
+                
             }
         }
-        string str="";
-        for(int i=start;i<=end+start-1;i++)
-        {
-            str.push_back(s[i]);
-        }
-        return str;
+        return s.substr(start,maxi);
     }
 };
