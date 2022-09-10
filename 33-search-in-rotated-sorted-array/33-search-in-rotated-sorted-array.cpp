e16393c13a01ee6b6ea97694a0d95b895fc548f3
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-09-10 17:35:51 +0530

    Time: 4 ms (73.49%), Space: 11.1 MB (74.97%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,18 +1,58 @@
-class Solution {
-public:
-    int search(vector<int>& nums, int target) {
-        
-        //linear searching O(n) and o(1);
-        
-        int n=nums.size();
-        
-        for(int i=0;i<n;i++)
+class Solution
+{
+    public:
+        int binary_search(vector<int> &nums, int low, int high, int target)
         {
-            if(nums[i]==target)
+            while (low <= high)
             {
-                return i;
+                int mid = (low + high) / 2;
+
+                if (nums[mid] == target)
+                {
+                    return mid;
+                }
+                else if (nums[mid] < target)
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid - 1;
+                }
+            }
+            return -1;
+        }
+    int search(vector<int> &nums, int target)
+    {
+
+       	//step 1->find the index of min element
+        int mini = nums[0];
+        int idx = 0;
+        int n=nums.size();
+        for (int i = 1; i < n; i++)
+        {
+            if (nums[i] < mini)
+            {
+                mini = nums[i];
+                idx = i;
             }
         }
-        return -1;
+
+        int left = binary_search(nums, 0, idx - 1, target);
+        int right = binary_search(nums, idx, n - 1, target);
+
+        if (left == -1)
+        {
+            return right;
+        }
+        else if (right == -1)
+        {
+            return left;
+        }
+        else
+
+        {
+            return -1;
+        }
     }
 };
