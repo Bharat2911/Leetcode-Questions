20eae829a81562eeeac22f54c4fc40c9d46331fb
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-09-10 15:13:31 +0530

    Time: 3 ms (72.34%), Space: 5.9 MB (73.74%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -2,6 +2,28 @@ class Solution {
 public:
     int mySqrt(int x) {
         
-        return pow(x,0.5);
+        //we will be applying binary search here
+        int ans=0;
+        
+        int low=1;
+        
+        int high=x;
+        
+       while(low<=high)
+       {
+           int mid=low+(high-low)/2;
+           
+           if(mid<=x/mid)//----> mid^2<=x
+           {
+               ans=mid;
+               low=mid+1;
+           }
+           else
+           {
+               high=mid-1;
+               
+           }
+       }
+        return ans;
     }
 };
