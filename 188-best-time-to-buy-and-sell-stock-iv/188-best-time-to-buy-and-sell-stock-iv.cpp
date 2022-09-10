db854382a183865af4230720c4cb30351de79298
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-09-10 12:31:41 +0530

    Time: 21 ms (48.09%), Space: 11.7 MB (66.59%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,37 +1,47 @@
 class Solution {
 public:
-     int f(int idx, bool isbuy, vector<int> &prices, int n, vector<vector< vector< int>>> &dp, int k)
+    int dp[1001][101][2];
+    
+    int solve(int idx,vector<int>&prices,int k,bool isbuy)
+    {
+        //bsae case
+        int n=prices.size();
+        
+        if(idx==n)
         {
-           	//base case
-            if (idx == n || k == 0)
-            {
-                return 0;
-            }
-
-            int profit = 0;
-
-            if (dp[idx][isbuy][k] != -1) return dp[idx][isbuy][k];
-
-           	//choice diagram
-            if (isbuy)
-            {
-               	//take or not take two choices here
-                profit = max(-prices[idx] + f(idx + 1, false, prices, n, dp, k), 0 + f(idx + 1, true, prices, n, dp, k));
-            }
-            else
-            {
-               	//whter to sell or not sell again two choices here
-                profit = max(prices[idx] + f(idx + 1, true, prices, n, dp, k - 1), 0 + f(idx + 1, false, prices, n, dp, k));
-            }
-
-            return dp[idx][isbuy][k] = profit;
+            return 0;
         }
+        if(k==0)
+        {
+            return 0;
+        }
+        
+        if(dp[idx][k][isbuy]!=-1)return dp[idx][k][isbuy];
+        
+        int profit=0;
+        
+        if(isbuy==true)
+        {
+            //will buy  or // not buy
+            profit=max(-prices[idx]+solve(idx+1,prices,k,false),solve(idx+1,prices,k,true));
+        }
+        else
+        {
+            profit=max(prices[idx]+solve(idx+1,prices,k-1,true),solve(idx+1,prices,k,false));
+        }
+        
+        return dp[idx][k][isbuy]=profit;
+    }
     int maxProfit(int k, vector<int>& prices) {
         
-        int n = prices.size();
-
-        vector<vector<vector< int>>> dp(n+1, vector<vector < int>> (3, vector<int> (k+1, -1)));
-
-        return f(0, true, prices, prices.size(), dp, k);
+        int n=prices.size();
+        
+        int idx=0;
+        
+        bool isbuy=true;
+        
+        memset(dp,-1,sizeof dp);
+        
+        return solve(idx,prices,k,isbuy);
     }
 };
