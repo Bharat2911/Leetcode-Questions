3342ef8bab22bb910bf349fd9313d8251e27f9fc
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-09-25 19:27:25 +0530

    Time: 131 ms (63.81%), Space: 50.4 MB (7.61%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,6 +1,5 @@
 class Solution {
 public:
-    
     int solve(int idx,vector<int>&coins,int sum,int n,vector<vector<int>>&dp)
     {
         if(idx==n)
@@ -12,13 +11,14 @@ public:
         {
             return 1;
         }
-        
         if(dp[idx][sum]!=-1)return dp[idx][sum];
+        
         
         if(coins[idx]<=sum)
         {
             return dp[idx][sum]=min(1+solve(idx,coins,sum-coins[idx],n,dp),solve(idx+1,coins,sum,n,dp));
         }
+        
         else
         {
             return dp[idx][sum]=solve(idx+1,coins,sum,n,dp);
@@ -26,20 +26,16 @@ public:
     }
     int coinChange(vector<int>& coins, int amount) {
         
+        int idx=0;
+        
         int n=coins.size();
         
         int sum=amount;
         
-        int idx=0;
-        
         vector<vector<int>>dp(n+1,vector<int>(sum+1,-1));
+        int ans=solve(idx,coins,sum,n,dp);
         
-        if(solve(idx,coins,sum,n,dp)>=1e9)
-        {
-            return -1;
-        }
-        
-        else return solve(idx,coins,sum,n,dp)-1;
-        
+        if(ans>=1e9)return -1;
+        else return ans-1;
     }
 };
