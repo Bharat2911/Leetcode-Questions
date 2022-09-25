75886bfb8acaca4f547b975d391cad2e4b2756a3
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-09-25 18:10:46 +0530

    Time: 38 ms (83.33%), Space: 23 MB (83.33%) - LeetHub

---
+++ 2418-sort-the-people/2418-sort-the-people.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        
+         vector<pair<int,string>>ans;
+        
+        for(int i=0;i<names.size();i++)
+        {
+            ans.push_back({heights[i],names[i]});
+        }
+        sort(ans.begin(),ans.end());
+        
+        reverse(ans.begin(),ans.end());
+        
+        vector<string>v;
+        
+        for(int i=0;i<ans.size();i++)
+        {
+            v.push_back(ans[i].second);
+        }
+        return v;
+    }
+};
