9722d82b79515dad65d98d201e0c074ac133ec6d
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-09-26 13:34:59 +0530

    Time: 232 ms (78.72%), Space: 76.2 MB (82.98%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -1,55 +1,64 @@
-class Solution {
-public:
-    int largestRectangleArea(vector<int>& height) {
-        
-        //for a particullar elemnet i have to find the previous smallest index and next smallest element index
-        stack<int>st;//and stack will store the index here
-        
-        int n=height.size();
-        
-        int left[n];
-        int right[n];
-        
-        
-        
-        for(int i=0;i<n;i++)
+class Solution
+{
+    public:
+        int largestRectangleArea(vector<int> &heights)
         {
-            while(!st.empty() and height[i]<=height[st.top()])
+
+            int n = heights.size();
+            stack<int> st;
+
+            int left[n];
+            int right[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                while (!st.empty() and heights[st.top()] >= heights[i])
+                {
+                    st.pop();
+                }
+
+                if (st.empty())
+                {
+                    left[i] = -1;
+                }
+                else
+                {
+                    left[i] = st.top();
+                }
+                st.push(i);
+            }
+            
+            //stack ko wapis se empty akr do taki next operatiosn ke liye use kar aaye
+            while(!st.empty())
             {
                 st.pop();
             }
-            if(st.empty())left[i]=-1;
-            else left[i]=st.top();
             
-            st.push(i);
-        }
-        
-        //now empty the stack so that we can gain use it in fingidn the nect smalles
-        while(!st.empty())
-        {
-            st.pop();
-        }
-        
-        
-        
-        for(int i=n-1;i>=0;i--)
-        {
-            while(!st.empty() and height[i]<=height[st.top()])
+            for(int i=n-1;i>=0;i--)
             {
-                st.pop();
+                while(!st.empty() and heights[st.top()] >= heights[i])
+                {
+                    st.pop();
+                }
+                
+                if(st.empty())
+                {
+                    right[i]=n;
+                }
+                else
+                {
+                    right[i]=st.top();
+                }
+                
+                st.push(i);
             }
-            if(st.empty())right[i]=n;
+            int ans=INT_MIN;
             
-            else right[i]=st.top();
+            for(int i=0;i<n;i++)
+            {
+                ans=max(ans,heights[i]*(right[i]-left[i]-1));
+            }
             
-            st.push(i);
+            return ans;
         }
-        int area=INT_MIN;
-        
-        for(int i=0;i<n;i++)
-        {
-            area=max(area,height[i]*(right[i]-left[i]-1));
-        }
-        return area;
-    }
 };
