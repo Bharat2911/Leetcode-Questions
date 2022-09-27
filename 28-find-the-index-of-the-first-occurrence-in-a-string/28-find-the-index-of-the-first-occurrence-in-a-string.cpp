2d41cd1ec22f498aa6e67aeee078b496e9f6ebc8
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-09-27 11:09:00 +0530

    Time: 3 ms (48.51%), Space: 6.5 MB (9.10%) - LeetHub

--- 28-find-the-index-of-the-first-occurrence-in-a-string/28-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/28-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,7 +1,48 @@
-class Solution {
-public:
-    int strStr(string str, string s) {
-        
-        return str.find(s);
-    }
+class Solution
+{
+    public:
+        int strStr(string str, string s)
+        {
+
+            int k = s.length();
+
+            if (s.length() == 0)
+            {
+                return -1;
+            }
+            if (str.length() == 0 and s.length() == 0)
+            {
+                return 0;
+            }
+            // if (str.length() == s.length())
+            // {
+            //     return 0;
+            // }
+            // if (s.length() > str.length())
+            // {
+            //     return -1;
+            // }
+
+            int i = 0;
+            int j = 0;
+
+            while (j < str.length())
+            {
+                if (j - i + 1 < k)	//case when window not hit
+                {
+                    j++;
+                }
+                else if (j - i + 1 == k)	//window get hit so in this case check for the answer
+                {
+                    if (str.substr(i, k) == s)
+                    {
+                       	//means match ho gya and answer aa rha hoga
+                        return i;	//since i is the pointing to the starting character of the string
+                    }
+                    i++;
+                    j++;
+                }
+            }
+            return -1;
+        }
 };
