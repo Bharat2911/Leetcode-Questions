80b4061c7c91f47f0ae8449b4eae3c50b40ca33a
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-09-28 19:27:27 +0530

    Time: 19 ms (43.00%), Space: 9.5 MB (16.74%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -1,33 +1,35 @@
-class Solution
-{
-    public:
-        vector<vector < int>> diagonalSort(vector<vector < int>> &mat)
+class Solution {
+public:
+    vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
+        
+        int row=mat.size();
+        int col=mat[0].size();
+        
+        vector<vector<int>>ans(row,vector<int>(col));
+        
+        //map ->int min head
+        // priority_queue<int,vector<int>,greater<int>>pq;
+        
+        unordered_map<int,  priority_queue<int,vector<int>,greater<int>>>map;
+        
+        for(int i=0;i<row;i++)
         {
-
-            int row = mat.size();
-            int col = mat[0].size();
-
-           	//will do uins map and min heap
-
-            unordered_map<int, priority_queue<int, vector< int >, greater < int>>> map;
-
-            for (int i = 0; i < row; i++)
+            for(int j=0;j<col;j++)
             {
-                for (int j = 0; j < col; j++)
-                {
-                    map[i - j].push(mat[i][j]);
-                }
+                map[i-j].push(mat[i][j]);
             }
-
-            for (int i = 0; i < row; i++)
+        }
+        
+       for (int i = 0; i < row; i++)
             {
                 for (int j = 0; j < col; j++)
                 {
-                    mat[i][j]=map[i-j].top();
+                    ans[i][j]=map[i-j].top();
                     map[i-j].pop();
                 }
 
             }
-            return mat;
-        }
+        return ans;
+        
+    }
 };
