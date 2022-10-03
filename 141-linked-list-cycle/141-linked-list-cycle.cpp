ff3bddd4673d7bbdadfa952adf0f6a2d1630aa1f
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-10-03 16:53:39 +0530

    Time: 15 ms (70.86%), Space: 8.1 MB (58.19%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -1,31 +1,30 @@
 /**
- *Definition for singly-linked list.
- *struct ListNode {
- *    int val;
- *    ListNode * next;
- *    ListNode(int x) : val(x), next(NULL) {}
- *};
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
  */
-class Solution
-{
-    public:
-        bool hasCycle(ListNode *head)
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        
+        //in this we will use the slow and fast pointer
+       ListNode*slow=head;
+        ListNode*fast=head;
+        
+        while(fast!=NULL and fast->next!=NULL)
+            
         {
-
-           	//rabbit and tortosi method
-            ListNode *slow = head;
-            ListNode *fast = head;
-
-            while (fast != NULL and fast->next != NULL)
+            fast=fast->next->next;
+            slow=slow->next;
+            
+            if(slow==fast)
             {
-                slow = slow->next;
-                fast = fast->next->next;
-
-                if (slow == fast)
-                {
-                    return true;
-                }
+                return true;
             }
-            return false;
         }
+        return false;
+    }
 };
