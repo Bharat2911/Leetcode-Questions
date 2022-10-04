e4ca596bcca08fe5aea5f4d4822c92be0b709d75
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-10-04 07:37:38 +0530

    Time: 7 ms (97.92%), Space: 22.3 MB (38.76%) - LeetHub

--- 112-path-sum/112-path-sum.cpp
+++ 112-path-sum/112-path-sum.cpp
@@ -1,46 +1,50 @@
 /**
- *Definition for a binary tree node.
- *struct TreeNode {
- *   int val;
- *   TreeNode * left;
- *   TreeNode * right;
- *   TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- *};
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
  */
-class Solution
-{
-    public:
-        bool ans;
-
-    void solve(TreeNode *root, int sum, int &target)
+class Solution {
+public:
+    void solve(TreeNode*root,unordered_map<int,int>&map,int sum)
     {
-        if (root == NULL) return;
-
-        sum += root->val;
-
-        if (root->left == NULL and root->right == NULL)
+        if(root==NULL)
         {
-            if (sum == target)
-            {
-                ans = true;
-            }
-            else
-            {
-                sum=0;
-            }
+            return;
         }
-        solve(root->left, sum, target);
-        solve(root->right, sum, target);
+        
+        sum+=root->val;
+        
+        if(root->left==NULL and root->right==NULL)
+        {
+            //means we encounter the leaf node
+            map[sum]++;
+        }
+        
+        solve(root->left,map,sum);
+        solve(root->right,map,sum);
+        
     }
-    bool hasPathSum(TreeNode *root, int targetSum)
-    {
-
-        int sum = 0;
-
-        solve(root, sum, targetSum);
-
-        return ans;
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        
+        if(root==NULL)return false;
+        
+        unordered_map<int,int>map;
+        
+        int sum=0;
+        
+        solve(root,map,sum);
+        
+        if(map.find(targetSum)!=map.end())
+        {
+            return true;
+        }
+        return false;
+        
     }
 };
