bbc0867d6f9cf5f53420e4ae7dd57e685eeb3bd4
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-10-06 12:51:00 +0530

    Time: 4 ms (78.34%), Space: 6.9 MB (24.88%) - LeetHub

--- 763-partition-labels/763-partition-labels.cpp
+++ 763-partition-labels/763-partition-labels.cpp
@@ -1,30 +1,38 @@
-class Solution {
-public:
-    vector<int> partitionLabels(string s) {
-        
-        unordered_map<char,int>map;
-        int n=s.length();
-        
-        //update the last position of each elelmnt int he ma[p
-        vector<int>ans;
-        int start=0;
-        int end=0;
-        
-        for(int i=0;i<n;i++)
+class Solution
+{
+    public:
+        vector<int> partitionLabels(string s)
         {
-            map[s[i]]=i;
-        }
-        
-        for(int i=0;i<n;i++)
-        {
-            end=max(end,map[s[i]]);
-            
-            if(i==end)
+
+           	//T.C=o(n)
+           	//S.C=O(n)
+
+           	//approach to store the last instance of each character
+            int n = s.length();
+
+            unordered_map<char, int> map;
+
+            for (int i = 0; i < n; i++)
             {
-                ans.push_back(end-start+1);
-                start=i+1;
+                map[s[i]] = i;
             }
+
+            vector<int> ans;
+
+            int start = 0;
+            int end = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                end = max(end, map[s[i]]);
+
+                if (i == end)
+                {
+                    ans.push_back(end - start + 1);
+
+                    start = i + 1;	//reset the start since i have taken my one substring
+                }
+            }
+            return ans;
         }
-        return ans;
-    }
 };
