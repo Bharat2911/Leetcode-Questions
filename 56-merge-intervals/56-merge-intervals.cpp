8c8c713e4ffedd5424a7a4ca16cf8a5a688be75b
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-10-07 12:03:36 +0530

    Time: 68 ms (55.79%), Space: 19.1 MB (69.72%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,29 +1,41 @@
-class Solution {
-public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        
-        sort(intervals.begin(),intervals.end());
-        
-        vector<vector<int>>ans;
-        
-        int l=intervals[0][0];
-        int r=intervals[0][1];
-        
-        for(int i=1;i<intervals.size();i++)
-            
+class Solution
+{
+    public:
+        vector<vector < int>> merge(vector<vector < int>> &intervals)
         {
-            if(intervals[i][0]<=r)
+            
+            //t>C=O(n log n)
+            //S.C=O(1)'
+            
+            vector<vector<int>>ans;
+
+            int j = 0;
+
+            int n = intervals.size();
+
+            sort(intervals.begin(), intervals.end());
+            
+            int l=intervals[0][0];
+            
+            int r=intervals[0][1];
+            
+            for(int i=1;i<n;i++)
             {
-                r=max(r,intervals[i][1]);
+                if(intervals[i][0]<=r)
+                {
+                    //means intervals overlapp ho rhe hai
+                    
+                    r=max(r,intervals[i][1]);
+                }
+                else
+                {
+                    ans.push_back({l,r});
+                    l=intervals[i][0];
+                    r=intervals[i][1];
+                }
             }
-            else
-            {
-                ans.push_back({l,r});
-                l=intervals[i][0];
-                r=intervals[i][1];
-            }
+            ans.push_back({l,r});
+            
+            return ans;
         }
-        ans.push_back({l,r});
-        return ans;
-    }
 };
