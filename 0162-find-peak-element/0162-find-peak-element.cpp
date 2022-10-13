52277128bb71f06c5a30529ffa3bfbcef92f4862
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-10-13 19:08:52 +0530

    Time: 6 ms (69.56%), Space: 8.6 MB (99.50%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -2,35 +2,48 @@ class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
         
-        
         int n=nums.size();
-        int ans=0;
         
-        if(n==2)
-        {
-            if(nums[1]>nums[0])return 1;
-            else return 0;
-        }
+        if(n==1)return 0;
         
-        if(n==1)
-        {
-            return 0;
-        }
+        int low=0;
+        int high=n-1;
         
-        for(int i=1;i<n-1;i++)
+        // int ans=0;
+        
+        while(low<=high)
         {
-            if(nums[i]>nums[i-1] and nums[i]>nums[i+1])
+            int mid=low+(high-low)/2;
+            
+            if(mid>0 and mid<n-1)
             {
-                ans=i;
+                if(nums[mid]>nums[mid-1] and nums[mid]>nums[mid+1])
+                {
+                    // ans=mid;
+                    return mid;
+                }
+                else if(nums[mid]<nums[mid-1])
+                {
+                    high=mid-1;
+                }
+                else if(nums[mid]<nums[mid+1])
+                {
+                    low=mid+1;
+                }
             }
-            if(nums[n-1]>nums[n-2] and i==n-2)
+            else if(mid==0 )
             {
-                ans=n-1;
+                if(nums[0]>nums[1])return 0;
+                else return 1;
+                
             }
-            // if(i==1 and nums[] )
+            else if(mid==n-1)
+            {
+                if(nums[n-1]>nums[n-2])return n-1;
+                else return n-2;
+                
+            }
         }
-        //check karo for last element and for first element
-        // if(nums[n-1]>nums[n-2] and )
-        return ans;
+        return -1;
     }
 };
