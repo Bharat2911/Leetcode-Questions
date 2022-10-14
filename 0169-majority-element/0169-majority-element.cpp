5c09bfed91674172be50a0a42f35a6b8ea8bf540
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-10-14 18:55:11 +0530

    Time: 43 ms (37.95%), Space: 19.6 MB (60.14%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -5,22 +5,36 @@ public:
         //T.C=O(n)
         //S.C=O(n)
         
+//         int n=nums.size();
+        
+//         unordered_map<int,int>map;
+        
+//         for(auto itr:nums)
+//         {
+//             map[itr]++;
+//         }
+        
+//         for(auto itr:map)
+//         {
+//             if(itr.second>n/2)
+//             {
+//                 return itr.first;
+//             }
+//         }
+//         return -1;
         int n=nums.size();
         
-        unordered_map<int,int>map;
+        int prev=0;
+        int count=0;
         
-        for(auto itr:nums)
+        for(int i=0;i<n;i++)
         {
-            map[itr]++;
+            if(count==0)prev=nums[i];
+            
+            if(prev==nums[i])count++;
+            
+            else count--;
         }
-        
-        for(auto itr:map)
-        {
-            if(itr.second>n/2)
-            {
-                return itr.first;
-            }
-        }
-        return -1;
+        return prev;
     }
 };
