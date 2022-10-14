0617973a120d658a890c9bd37398f9a818cc335c
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-10-15 00:45:24 +0530

    Time: 377 ms (53.54%), Space: 64.8 MB (93.79%) - LeetHub

---
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
+        
+        
+        //make the adj list then simply apply the dijstrax algo man
+        vector<pair<int,double>>adj[n];
+         bool found=false;
+        for(int i=0;i<edges.size();i++)
+        {
+            int u=edges[i][0];
+            int v=edges[i][1];
+            
+            double w=succProb[i];
+            
+            adj[u].push_back({v,w});
+            adj[v].push_back({u,w});
+            
+             if(edges[i][0]==end ||edges[i][1]==end)found=true;
+        }
+        if(!found)return false;
+        vector<double>dist(n,INT_MIN);
+        
+        priority_queue<pair<double,int>>pq;
+        //->dist,node
+        
+        dist[start]=1.0;
+        pq.push({1.0,start});
+        
+        while(!pq.empty())
+        {
+            double d=pq.top().first;
+            int node=pq.top().second;
+            
+            pq.pop();
+            
+            for(auto itr:adj[node])
+            {
+                int u=itr.first;
+                double w=itr.second;
+                
+                if(dist[u]<dist[node]*w)
+                {
+                    dist[u]=dist[node]*w;
+                    
+                    pq.push({dist[u],u});
+                }
+            }
+        }
+        if(dist[end]==INT_MIN)return 0;
+        return dist[end];
+    }
+};
