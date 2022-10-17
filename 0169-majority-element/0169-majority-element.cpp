59525643a8a1a10cfed8dd04586b0c0e94681931
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-10-17 22:12:31 +0530

    Time: 54 ms (13.95%), Space: 19.5 MB (92.20%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -2,39 +2,20 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         
-        //T.C=O(n)
-        //S.C=O(n)
-        
-//         int n=nums.size();
-        
-//         unordered_map<int,int>map;
-        
-//         for(auto itr:nums)
-//         {
-//             map[itr]++;
-//         }
-        
-//         for(auto itr:map)
-//         {
-//             if(itr.second>n/2)
-//             {
-//                 return itr.first;
-//             }
-//         }
-//         return -1;
+        //same as mores voting algorithmn 
         int n=nums.size();
         
-        int prev=0;
         int count=0;
+        int prev=0;
         
         for(int i=0;i<n;i++)
         {
             if(count==0)prev=nums[i];
             
             if(prev==nums[i])count++;
-            
             else count--;
         }
+        
         return prev;
     }
 };
