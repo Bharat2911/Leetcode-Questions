ee1efedece9a64e4aaede927fd545ea0add2f541
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-10-28 13:28:10 +0530

    Time: 130 ms (58.61%), Space: 33.6 MB (48.64%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
@@ -2,36 +2,26 @@ class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
         
-        //brute force->sort
+        //brute force->sorting
         //T.C=O(nlogn)
         //S.C=O(1);
         
-        sort(nums.begin(),nums.end());
+//         sort(nums.begin(),nums.end());
         
-        vector<int>ans;
+//         vector<int>ans;
         
-        for(int i=0;i<nums.size()-1;i++)
-        {
-            if(nums[i]==nums[i+1])
-            {
-                ans.push_back(nums[i]);
-            }
-        }
+//         for(int i=0;i<nums.size()-1;i++)
+//         {
+//             if(nums[i]==nums[i+1])
+//             {
+//                 ans.push_back(nums[i]);
+//             }
+//         }
         
-        return ans;
+//         return ans;
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        //using map man
+       
+        //optimisation->using map (hashing)
         
         //T.C=O(n);
         //S.C=O(n);
@@ -52,9 +42,31 @@ public:
 //                 ans.push_back(itr.first);
 //             }
 //         }
-        
-        
-        
 //         return ans;
+        
+        
+        //most optimised
+        //first time element seen hoga usko negative kar do yahi approach hai my friend
+        
+        //T.C=O(n)
+        //S.c=O(1);
+        
+        vector<int>ans;
+        for(auto itr:nums)
+        {
+            itr=abs(itr);
+            
+            if(nums[itr-1]>0)
+            {
+                nums[itr-1]*=-1;
+            }
+            else
+            {
+                ans.push_back(itr);
+            }
+        }
+        return ans;
+        
+       
     }
 };
