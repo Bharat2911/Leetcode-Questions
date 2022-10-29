d8debc8f7d4d8475e4bec1f2b0dda3602d371c14
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-10-29 09:45:12 +0530

    Time: 73 ms (89.50%), Space: 33.5 MB (93.75%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
@@ -1,57 +1,10 @@
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
-        
-        //brute force->sorting
-        //T.C=O(nlogn)
-        //S.C=O(1);
-        
-//         sort(nums.begin(),nums.end());
-        
-//         vector<int>ans;
-        
-//         for(int i=0;i<nums.size()-1;i++)
-//         {
-//             if(nums[i]==nums[i+1])
-//             {
-//                 ans.push_back(nums[i]);
-//             }
-//         }
-        
-//         return ans;
-        
-       
-        //optimisation->using map (hashing)
-        
-        //T.C=O(n);
-        //S.C=O(n);
-        
-//         unordered_map<int,int>map;
-        
-//         for(auto itr:nums)
-//         {
-//             map[itr]++;
-//         }
-        
-//         vector<int>ans;
-        
-//         for(auto itr:map)
-//         {
-//             if(itr.second==2)
-//             {
-//                 ans.push_back(itr.first);
-//             }
-//         }
-//         return ans;
-        
-        
-        //most optimised
-        //first time element seen hoga usko negative kar do yahi approach hai my friend
-        
-        //T.C=O(n)
-        //S.c=O(1);
+        //negative mark karte jao simple 
         
         vector<int>ans;
+        
         for(auto itr:nums)
         {
             itr=abs(itr);
@@ -59,6 +12,7 @@ public:
             if(nums[itr-1]>0)
             {
                 nums[itr-1]*=-1;
+                
             }
             else
             {
@@ -66,7 +20,5 @@ public:
             }
         }
         return ans;
-        
-       
     }
 };
