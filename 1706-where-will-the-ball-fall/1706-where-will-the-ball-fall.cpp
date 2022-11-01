e3b4ce075c9590651821f7c2defbb60eefcc6d73
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-11-01 23:39:38 +0530

    Time: 39 ms (74.01%), Space: 13.2 MB (52.18%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -1,41 +1,49 @@
-class Solution
-{
-    public:
-        bool valid(int rsize, int csize, int r, int c)
-        {
-            if (rsize <= r || csize <= c || c < 0 || r < 0) return false;
-            return true;
-        }
-    int DFS(vector<vector < int>> &grid, int r, int c)
+class Solution {
+public:
+    bool is_safe(vector<vector<int>>&grid,int i,int j,int row,int col)
     {
-        if (r == grid.size()) return c;
-        if (valid(grid.size(), grid[0].size(), r, c))
+        if(i<0 || i>=row || j<0 || j>=col)return false;
+        
+        return true;
+    }
+    int dfs(vector<vector<int>>&grid,int i,int j,int row,int col)
+    {
+        //if i reach the ending row then i will get my answer
+        if(i==row)return j;
+        
+        if(is_safe(grid,i,j,row,col))
         {
-            if (grid[r][c] == 1)
+            if(grid[i][j]==1)
             {
-                if (valid(grid.size(), grid[0].size(), r, c + 1) && grid[r][c + 1] == 1)
+                //out og bounf nahi hona chaiye and stuck condition nahi honi chaiye
+                if(is_safe(grid,i,j+1,row,col) and grid[i][j+1]==1)
                 {
-                    return DFS(grid, r + 1, c + 1);
+                    return dfs(grid,i+1,j+1,row,col);
                 }
             }
             else
             {
-                if (valid(grid.size(), grid[0].size(), r, c - 1) && grid[r][c - 1] == -1)
+                if(is_safe(grid,i,j-1,row,col) and grid[i][j-1]==-1)
                 {
-                    return DFS(grid, r + 1, c - 1);
+                    return dfs(grid,i+1,j-1,row,col);
                 }
             }
         }
         return -1;
     }
-    vector<int> findBall(vector<vector < int>> &grid)
-    {
-        int row = grid.size(), col = grid[0].size();
-        vector<int> ans(col, 0);
-        for (int c = 0; c < col; c++)
+    vector<int> findBall(vector<vector<int>>& grid) {
+        
+        int row=grid.size();
+        
+        int col=grid[0].size();
+        
+        vector<int>ans(col,0);
+        
+        for(int i=0;i<col;i++)
         {
-            ans[c] = DFS(grid, 0, c);
+            ans[i]=dfs(grid,0,i,row,col);
         }
+        
         return ans;
     }
 };
