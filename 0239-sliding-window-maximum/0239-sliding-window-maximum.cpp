e61eafd60ce864592c7ace958d55d1f0bcaa57e0
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-11-25 11:00:03 +0530

    Time: 694 ms (41.20%), Space: 134.8 MB (41.34%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -2,39 +2,41 @@ class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         
-        int n=nums.size();
+        //main motive maintaing decreasing order in deque
         
-        vector<int>ans;
+        int n=nums.size();
         
         deque<int>dq;
         
-        //k size subarray
+        vector<int>ans;
+        
         for(int i=0;i<k;i++)
         {
             while(!dq.empty() and nums[i]>dq.back())
             {
                 dq.pop_back();
+                
             }
-            
             dq.push_back(nums[i]);
         }
         ans.push_back(dq.front());
         
         for(int i=k;i<n;i++)
         {
-            //current elemnt is not the elemnt of previous windo0w
+            //check current ele is not the ele of previous window
+            
             if(dq.front()==nums[i-k])
             {
                 dq.pop_front();
             }
+            
             while(!dq.empty() and nums[i]>dq.back())
             {
                 dq.pop_back();
+                
             }
-            
             dq.push_back(nums[i]);
             ans.push_back(dq.front());
-            
         }
         return ans;
     }
