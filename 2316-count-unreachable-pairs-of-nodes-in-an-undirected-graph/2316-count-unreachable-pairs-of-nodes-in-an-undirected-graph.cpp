d7a08b579bf686cb27a4aa95fe052661ddefed3e
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-12-06 10:23:53 +0530

    Time: 1558 ms (12.21%), Space: 298.1 MB (5.73%) - LeetHub

--- 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
+++ 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
-    //approach ->count the components and the elemnts in each component using bfs/dfs
-    //then simply count the possible pairs
-    void bfs(int start,vector<int>adj[],vector<int>&vis,int n,vector<int>&v)
+    void solve(int start,vector<int>adj[],vector<int>&vis,vector<int>&v)
     {
         queue<int>q;
         q.push(start);
@@ -13,7 +11,7 @@ public:
         {
             int node=q.front();
             q.pop();
-            count++;//counting the elements in each components
+            count++;
             
             for(auto itr:adj[node])
             {
@@ -23,13 +21,13 @@ public:
                     q.push(itr);
                 }
             }
+           
         }
-        v.push_back(count);
+         v.push_back(count);
     }
     long long countPairs(int n, vector<vector<int>>& edges) {
         
         vector<int>adj[n];
-        vector<int>v;
         
         for(auto itr:edges)
         {
@@ -40,30 +38,27 @@ public:
             adj[v].push_back(u);
         }
         
-        int components=0;
-        
         vector<int>vis(n,0);
-        
+        int components=0;
+        vector<int>v;
         for(int i=0;i<n;i++)
         {
             if(!vis[i])
             {
                 components++;
-                bfs(i,adj,vis,n,v);
+                solve(i,adj,vis,v);
             }
         }
-        long long int ans=0;
-        long long int pro;
-        
-        
+        long long ans=0;
+        long long pro;
         
         for(int i=0;i<v.size();i++)
         {
-             pro=1ll*v[i]*(n-v[i]);
+            pro=(long long)v[i]*(n-v[i]);
+            
             ans+=pro;
         }
         if(components==1)return 0;
-        
-        return ans/2ll;
+        else return ans/(long long)2;
     }
 };
