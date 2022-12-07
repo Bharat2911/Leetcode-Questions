9e0487769f04650920abdfefd14f209be1d04ef4
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-12-07 10:49:37 +0530

    Time: 162 ms (78.04%), Space: 64.7 MB (18.63%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
@@ -1,50 +1,45 @@
 /**
- *Definition for a binary tree node.
- *struct TreeNode {
- *   int val;
- *   TreeNode * left;
- *   TreeNode * right;
- *   TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- *};
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
  */
-class Solution
-{
-    public:
-        void solve(TreeNode *root, vector<int> &inorder)
-        {
-            if (root == NULL)
-            {
-                return;
-            }
-            solve(root->left, inorder);
-
-            inorder.push_back(root->val);
-
-            solve(root->right, inorder);
-        }
-    int rangeSumBST(TreeNode *root, int low, int high)
+class Solution {
+public:
+    int ans=0;
+    
+    void solve(TreeNode*root,int low,int high)
     {
-
-       	//approach 1->T.C=O(h) S.C=O(h)
-        vector<int> inorder;
-
-        if (root == NULL)
+        if(root==NULL)
+        {
+            return;
+        }
+        
+        if(root->val>=low and root->val<=high)
+        {
+            ans+=root->val;
+        }
+        
+        solve(root->left,low,high);
+        solve(root->right,low,high);
+        
+        
+    }
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        
+        //approach 2->T.C=O(h) S.C=O(1)
+        
+        if(root==NULL)
         {
             return 0;
         }
-
-        solve(root, inorder);
-        int ans = 0;
-
-        for (int i = 0; i < inorder.size(); i++)
-        {
-            if (inorder[i] >= low and inorder[i] <= high)
-            {
-                ans += inorder[i];
-            }
-        }
+        solve(root,low,high);
+        
         return ans;
     }
 };
