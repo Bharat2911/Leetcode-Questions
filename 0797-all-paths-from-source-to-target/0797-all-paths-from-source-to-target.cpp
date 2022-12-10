51e25c0add15c1ff89986aef1988a5ca3705d2e2
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-12-10 14:05:39 +0530

    Time: 50 ms (25.27%), Space: 15.3 MB (29.08%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -1,40 +1,45 @@
-class Solution
-{
-    public:
-    //approach->dfs/bfs+backtracking man
-        void dfs(int start, vector<vector < int>> &graph, vector< vector< int >> &ans, vector< int > path, int target)
-        {
-            path.push_back(start);
-
-           	//base case meri jaan
-            if (start == target)
-            {
-                ans.push_back(path);
-                return;
-            }
-            else
-            {
-               	//itrate into adjacent 
-                for (auto itr: graph[start])
-                {
-                    dfs(itr, graph, ans, path, target);
-                }
-            }
-            // path.pop_back();	//backtracking step meri jaan
-        }
-    vector<vector < int>> allPathsSourceTarget(vector<vector < int>> &graph)
+class Solution {
+public:
+    void solve(int start,vector<vector<int>>&adj,vector<vector<int>>&ans,vector<int>path,int &target)
     {
-
-        int n = graph.size();
-
-        vector<vector < int>> ans;
-
-        vector<int> path;
-
-        int target = n - 1;
-
-        dfs(0, graph, ans, path, target);
-
+        path.push_back(start);
+        
+        if(start==target)
+        {
+            ans.push_back(path);
+            return;
+        }
+        else
+        {
+            for(auto itr:adj[start])
+            {
+                solve(itr,adj,ans,path,target);
+            }
+        }
+        path.pop_back();
+    }
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        
+        //make the adj list
+        int n=graph.size();
+        
+        vector<vector<int>>adj(n);
+        int target=n-1;
+        int start=0;
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<graph[i].size();j++)
+            {
+                adj[i].push_back(graph[i][j]);
+            }
+        }
+        
+        vector<vector<int>>ans;
+        vector<int>path;
+        
+        solve(start,adj,ans,path,target);
+        
         return ans;
     }
 };
