3867d1e59be228f7a0f95a8ed8185696e9bad0a8
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-12-10 22:44:52 +0530

    Time: 6 ms (50.00%), Space: 7.9 MB (30.00%) - LeetHub

---
+++ 2496-maximum-value-of-a-string-in-an-array/2496-maximum-value-of-a-string-in-an-array.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int maximumValue(vector<string>& strs) {
+        
+        priority_queue<int>pq;//max heap
+        int num=0;
+        
+        for(auto itr:strs)
+        {
+            int a=0;
+            int d=0;
+            
+           for(int i=0;i<itr.length();i++)
+           {
+               if(isalpha(itr[i]))
+               {
+                   a++;
+                   
+               }
+               else 
+               {
+                   d++;
+                   
+               }
+           }
+           
+           if(a==0 and d>0)
+            {
+                for(auto x:itr)
+                {
+                    num=num*10+x-'0';
+                    
+                }
+               pq.push(num);
+               num=0;
+              
+            }
+            else
+            {
+                pq.push(itr.length());
+            }
+        }
+        return pq.top();
+    }
+};
