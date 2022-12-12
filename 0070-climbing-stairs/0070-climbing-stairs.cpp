dca404debfe63f0cb6634cf4601560a401501b36
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-12-12 17:41:47 +0530

    Time: 4 ms (21.07%), Space: 6.2 MB (29.54%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,33 +1,16 @@
 class Solution {
 public:
-    //recursion solution
-    int dp[46];
-    
-    int solve(int n)
-    {
-        //base case
-        if(n==0)
-        {
-            return 1;
-        }
-        if(n==1)
-        {
-            return 1;
-        }
-        
-        if(dp[n]!=-1)return dp[n];
-        
-        //cjpice diagram
-        int a=solve(n-1);
-        
-        int b=solve(n-2);
-        
-        return dp[n]=a+b;
-    }
     int climbStairs(int n) {
         
-        memset(dp,-1,sizeof dp);
+        vector<int>dp(n+1);
         
-        return solve(n);
+        dp[0]=1;
+        dp[1]=1;
+        
+        for(int i=2;i<=n;i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
