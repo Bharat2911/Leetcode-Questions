be036909c2e2daf9852577f370ca9c17a0ff410e
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-12-17 00:57:22 +0530

    Time: 176 ms (42.46%), Space: 25.4 MB (26.98%) - LeetHub

---
+++ 2207-maximize-number-of-subsequences-in-a-string/2207-maximize-number-of-subsequences-in-a-string.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    long long solve(string&s,string&p)
+    {
+         int n=s.length(); 
+        
+         long long ans=0;
+        unordered_map<char,long long>map;
+        
+        for(auto itr:s)
+        {
+            if(itr==p[1])
+            {
+                ans+=map[p[0]];
+            }
+            map[itr]++;
+        }
+        return ans;
+    }
+    long long maximumSubsequenceCount(string text, string pattern) {
+        
+        //pattern ka second hamesha end me lagao aur pattern ka first char hamesha starting me to get maximum answer
+        //T.C=O(n)//linearly itrating
+        //S.C=O(n)//using the suffix array
+        
+        string str1=text+pattern[1];//O(1)
+        
+        string str2=pattern[0]+text;//O(1)
+        
+        long long ans1=solve(str1,pattern);//O(n),O(n)
+        long long ans2=solve(str2,pattern);//O(n),O(n)
+        
+        return max(ans1,ans2);
+    }
+};
