d4483b0e4cc66d72898f06bb8bac1bbcb0c564e3
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-12-19 15:34:46 +0530

    Time: 122 ms (17.91%), Space: 27.5 MB (92.47%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -4,15 +4,39 @@ public:
         
         //brute force ->o(n),O(1) (linear searching)
         
-        int ans=-1;
+//         int ans=-1;
         
-        for(int i=0;i<nums.size();i++)
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             if(nums[i]==target)
+//             {
+//                 ans=i;
+//             }
+//         }
+//         return ans;
+        
+        //approach->2 optimised O(log n)/O(1)
+        //binary searching 
+        
+        int n=nums.size();
+        
+        int low=0;
+        int high=n-1;
+        
+        while(low<=high)
         {
-            if(nums[i]==target)
+            int mid=low+(high-low)/2;
+            
+            if(nums[mid]==target)return mid;
+            else if(nums[mid]>target)
             {
-                ans=i;
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
             }
         }
-        return ans;
+        return -1;
     }
 };
