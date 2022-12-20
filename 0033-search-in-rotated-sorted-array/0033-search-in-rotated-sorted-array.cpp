d99092f5369cf6096ca3a2e81dbc2dcff218de86
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-12-20 15:56:17 +0530

    Time: 0 ms (100.00%), Space: 11.1 MB (30.62%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,13 +1,62 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
+        
+        //approach 1->linear searching 
+        //O(n)
+        //O(1)
+        
+//         int n=nums.size();
+        
+//         for(int i=0;i<n;i++)
+//         {
+//             if(nums[i]==target)
+//             {
+//                 return i;
+//             }
+//         }
+//         return -1;
+        
+        //approach 2->binary search 
+        //O(log n)
+        //O(1)
         int n=nums.size();
         
-        for(int i=0;i<n;i++)
+        int low=0;
+        int high=n-1;
+        
+        while(low<=high)
         {
-            if(nums[i]==target)
+            int mid=low+(high-low)/2;
+            
+            if(nums[mid]==target)
             {
-                return i;
+                return mid;
+            }
+            //we have two cases either we have left sorted array or we have right sorted array
+            else if(nums[mid]>=nums[low])
+            {
+                //left sorted array
+                if(target>=nums[low] and target<nums[mid])
+                {
+                    high=mid-1;
+                }
+                else
+                {
+                    low=mid+1;
+                }
+            }
+            else
+            {
+                //right sorted array
+                if(target>nums[mid] and target<=nums[high])
+                {
+                    low=mid+1;
+                }
+                else
+                {
+                    high=mid-1;
+                }
             }
         }
         return -1;
