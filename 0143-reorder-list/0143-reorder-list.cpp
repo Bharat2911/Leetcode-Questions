9a9ecc536d5cdcbf748195e6c0232289b3afc81b
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2022-12-23 18:50:05 +0530

    Time: 50 ms (78.55%), Space: 17.7 MB (76.34%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -0,0 +1,66 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode*reverse(ListNode*head)
+    {
+        ListNode*curr=head;
+        ListNode*prev=NULL;
+        ListNode*next;
+        
+        while(curr!=NULL)
+        {
+            next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+            
+        }
+        return prev;
+    }
+    void merge(ListNode*l1,ListNode*l2)
+    {
+        while(l2)
+        {
+            ListNode*next=l1->next;
+            
+            l1->next=l2;
+            l1=l2;
+            l2=next;
+        }
+    }
+    void reorderList(ListNode* head) {
+        
+        if(head==NULL || head->next==NULL)
+        {
+            return ;
+        }
+        ListNode*fast=head;
+        
+        ListNode*slow=head;
+        
+        ListNode*prev=head;
+        
+        while(fast!=NULL and fast->next!=NULL)
+        {
+            prev=slow;
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        prev->next=NULL;
+        
+        //reverse second half
+        ListNode*l1=head;
+        ListNode*l2=reverse(slow);
+        
+        merge(l1,l2);
+    }
+};
