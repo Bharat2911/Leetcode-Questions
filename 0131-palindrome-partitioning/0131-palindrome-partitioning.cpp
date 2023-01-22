7b7e0256ce32aab2d1bdcdb603c6c5be1ecc3288
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2023-01-22 11:24:32 +0530

    Time: 220 ms (40.48%), Space: 49.2 MB (90.40%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    bool ispal(string &s,int low,int high)
+    {
+        while(low<=high)
+        {
+            if(s[low++]!=s[high--])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    void solve(int idx,string&s,vector<vector<string>>&ans,vector<string>&res)
+    {
+        //base case
+        if(idx==s.length())
+        {
+            ans.push_back(res);
+            return;
+        }
+        
+        for(int i=idx;i<s.length();i++)
+        {
+            if(ispal(s,idx,i))
+            {
+                res.push_back(s.substr(idx,i-idx+1));
+            
+            solve(i+1,s,ans,res);
+            
+            res.pop_back();//backtracking step
+            }    
+            
+        }
+    }
+    vector<vector<string>> partition(string s) {
+        
+        int n=s.length();
+        
+        vector<vector<string>>ans;
+        vector<string>res;
+        
+        int idx=0;
+        
+        solve(idx,s,ans,res);
+        
+        return ans;
+    }
+};
