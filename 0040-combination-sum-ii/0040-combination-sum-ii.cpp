6ca2da92086c7eeb78487f108210ee64fe960008
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2023-08-04 11:22:05 +0530

    Time: 3 ms (89.37%), Space: 10.5 MB (75.53%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,37 +1,49 @@
-class Solution
-{
-    public:
-        void solve(int idx, vector<int> &arr, vector<vector< int >> &ans, vector< int > &res, int target)
+class Solution {
+public:
+    void solve(int idx,vector<int>&nums,vector<vector<int>>&ans,vector<int>&res,int sum)
+    {
+        //base case
+        if(idx==nums.size())
         {
-            if (target == 0)
+            if(sum==0)
             {
                 ans.push_back(res);
-                return;
             }
-            for (int i = idx; i < arr.size(); i++)
-            {
-                if (i > idx and arr[i] == arr[i - 1]) continue;
-
-               	// if (arr[i]>target)break;
-                if (arr[i] <= target)
-                {
-                    res.push_back(arr[i]);
-                    solve(i + 1, arr, ans, res, target - arr[i]);
-                    res.pop_back();
-                }
-            }
+            return;
         }
-    vector<vector < int>> combinationSum2(vector<int> &arr, int target)
-    {
-
-        sort(arr.begin(), arr.end());
-
-        vector<vector < int>> ans;
-
-        vector<int> res;
-
-        solve(0, arr, ans, res, target);
-
+        
+        //pick
+        if(nums[idx]<=sum)
+        {
+            res.push_back(nums[idx]);
+            
+            solve(idx+1,nums,ans,res,sum-nums[idx]);
+            
+            //once recursion call is finished then i have to undo the changes
+            
+            res.pop_back();
+        }
+        
+        //not pick
+        //if i am not picking then i have to not pick all the occurance of current element
+        while(idx+1<nums.size() and nums[idx]==nums[idx+1])idx++;
+        
+        solve(idx+1,nums,ans,res,sum);
+    }
+    vector<vector<int>> combinationSum2(vector<int>& nums, int sum) {
+        
+        //concept is this if we skip then we skipp all occurance of that current element
+        
+        vector<vector<int>>ans;
+        
+        vector<int>res;
+        
+        int idx=0;
+        
+        sort(nums.begin(),nums.end());
+        
+        solve(idx,nums,ans,res,sum);
+        
         return ans;
     }
 };
