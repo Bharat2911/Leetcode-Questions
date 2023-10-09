811518e082fe67d921b4ed67eb90712454c43159
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2023-10-09 11:58:32 +0530

    Time: 3 ms (91.96%), Space: 13.9 MB (37.79%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -5,37 +5,64 @@ public:
         //approach 1->linear searching we will do here
         //approch 2->we can do this using the binary searching
         
+//         int n=nums.size();
+        
+//         vector<int>ans;
+        
+//         for(int i=0;i<n;i++)
+//         {
+//             if(nums[i]==target)
+//             {
+//                 ans.push_back(i);
+//                 break;
+//             }
+            
+//         }
+//         if(ans.size()==0)
+//         {
+//             ans.push_back(-1);
+//         }
+        
+//         for(int i=n-1;i>=0;i--)
+//         {
+//             if(nums[i]==target)
+//             {
+//                 ans.push_back(i);
+//                 break;
+//             }
+//         }
+//         if(ans.size()==1)
+//         {
+//             ans.push_back(-1);
+//         }
+        
+//         return ans;
+        
+        //approach 2
+        
         int n=nums.size();
         
-        vector<int>ans;
+        vector<int>ans(2);
         
-        for(int i=0;i<n;i++)
-        {
-            if(nums[i]==target)
-            {
-                ans.push_back(i);
-                break;
-            }
-            
-        }
-        if(ans.size()==0)
-        {
-            ans.push_back(-1);
-        }
+         bool p=binary_search(nums.begin(), nums.end(), target);
         
-        for(int i=n-1;i>=0;i--)
+        if(!p)
         {
-            if(nums[i]==target)
-            {
-                ans.push_back(i);
-                break;
-            }
+            return {-1,-1};
         }
-        if(ans.size()==1)
-        {
-            ans.push_back(-1);
-        }
+        int lower=lower_bound(nums.begin(),nums.end(),target)-nums.begin();
+        
+        int upper=upper_bound(nums.begin(),nums.end(),target)-nums.begin();
+        
+        upper=upper-1;
+        
+        
+        ans[0]=lower;
+        
+        ans[1]=upper;
+        
         
         return ans;
+        
     }
 };
