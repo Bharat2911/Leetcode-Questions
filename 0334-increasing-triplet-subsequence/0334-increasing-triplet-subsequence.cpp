70f7fdc4a17abd4d3bec506d1fb6b2159e6275c4
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2023-10-12 19:08:55 +0530

    Time: 114 ms (11.76%), Space: 121.4 MB (5.77%) - LeetHub

---
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        
+        //approch 1-> T.C->O(n^3)
+        //         -> S.C->O(1);
+        
+        
+        //approch 2->T.C->O(n)
+        //         ->S.C->O(n)
+        
+        int n=nums.size();
+        
+        vector<int>left(n);
+        vector<int>right(n);
+        
+        left[0]=nums[0];
+        
+        right[n-1]=nums[n-1];
+        
+        for(int i=1;i<n;i++)
+        {
+            left[i]=min(left[i-1],nums[i]);
+        }
+        
+        for(int i=n-2;i>=0;i--)
+        {
+            right[i]=max(right[i+1],nums[i]);
+        }
+        
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]>left[i] and nums[i]<right[i])
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
