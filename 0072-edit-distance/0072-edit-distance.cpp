8f2de1dcec572858680608f55b038f13f133f2e7
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2023-10-24 00:38:48 +0530

    Time: 3 ms (97.39%), Space: 9.5 MB (27.12%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -11,39 +11,37 @@ public:
             return i+1;
         }
         
-        if(s1[i]==s2[j])
-        {
-            return solve(i-1,j-1,s1,s2,dp);
-        }
-        
         if(dp[i][j]!=-1)
         {
             return dp[i][j];
-            
         }
-        else
+        
+        if(s1[i]==s2[j])
         {
-            int a=1+solve(i-1,j,s1,s2,dp);
-            int b=1+solve(i,j-1,s1,s2,dp);
-            int c=1+solve(i-1,j-1,s1,s2,dp);
-            
-            return dp[i][j]=min({a,b,c});
+            return dp[i][j]=solve(i-1,j-1,s1,s2,dp);//isme count nahi karenge kyuki isme koi operations use nahi ho rhe boss
         }
+        
+        /// we have to give the minimum operations so operations jab use hoga tab hi to usko count karenge na dost
+        
+        int a=1+solve(i-1,j,s1,s2,dp);
+        
+        int b=1+solve(i,j-1,s1,s2,dp);
+        
+        int c=1+solve(i-1,j-1,s1,s2,dp);
+        
+        return dp[i][j]=min({a,b,c});
     }
     int minDistance(string s1, string s2) {
         
         int n=s1.length();
+        
         int m=s2.length();
         
         int i=n-1;
         int j=m-1;
         
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        
-        
+        vector<vector<int>>dp(n,vector<int>(m,-1));
         
         return solve(i,j,s1,s2,dp);
-        
-        
     }
 };
