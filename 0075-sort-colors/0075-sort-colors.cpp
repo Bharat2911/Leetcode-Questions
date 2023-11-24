47ac26b0d5336620c5ae2805b409d768e1eba6df
Bharat singh <96980832+Bharat2911@users.noreply.github.com>
2023-11-24 19:46:14 +0530

    Time: 2 ms (42.69%), Space: 8.6 MB (36.14%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -5,22 +5,32 @@ public:
         int n=nums.size();
         
         int low=0;
+        
         int mid=0;
+        
         int high=n-1;
+        
         
         while(mid<=high)
         {
-            switch(nums[mid])
+            //case 1
+            if(nums[mid]==0)
             {
-                case 0:
-                    swap(nums[low++],nums[mid++]);
-                    break;
-                case 1:
-                    mid++;
-                    break;
-                case 2:
-                    swap(nums[mid],nums[high--]);
-                    break;
+                swap(nums[mid],nums[low]);
+                mid++;
+                low++;
+            }
+            
+            //case 2
+            else if(nums[mid]==1)
+            {
+                mid++;
+            }
+            //case 3
+            else
+            {
+                swap(nums[mid],nums[high]);
+                high--;
             }
         }
     }
